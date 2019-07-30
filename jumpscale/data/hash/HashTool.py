69f05d3327852888a1ec33fd2b5d1660b2575902
omarelsheekh <31706237+omarelsheekh@users.noreply.github.com>
2019-07-30 16:37:22 +0200

    added support for larg files

--- jumpscale/data/hash/HashTool.py
+++ jumpscale/data/hash/HashTool.py
@@ -19,61 +19,81 @@ def get_list_files(dir_name):
 
     return all_files
 
-def md5(string,encode='utf-8'):
+
+def md5(string, encode="utf-8"):
     return hashlib.md5(string.encode(encode)).hexdigest()
 
-def sha1(string,encode='utf-8'):
+
+def sha1(string, encode="utf-8"):
     return hashlib.sha1(string.encode(encode)).hexdigest()
 
-def sha224(string,encode='utf-8'):
+
+def sha224(string, encode="utf-8"):
     return hashlib.sha224(string.encode(encode)).hexdigest()
 
-def sha384(string,encode='utf-8'):
+
+def sha384(string, encode="utf-8"):
     return hashlib.sha384(string.encode(encode)).hexdigest()
 
-def sha256(string,encode='utf-8'):
+
+def sha256(string, encode="utf-8"):
     return hashlib.sha256(string.encode(encode)).hexdigest()
 
-def sha512(string,encode='utf-8'):
+
+def sha512(string, encode="utf-8"):
     return hashlib.sha512(string.encode(encode)).hexdigest()
 
-def sha3_224(string,encode='utf-8'):
+
+def sha3_224(string, encode="utf-8"):
     return hashlib.sha3_224(string.encode(encode)).hexdigest()
 
-def sha3_256(string,encode='utf-8'):
+
+def sha3_256(string, encode="utf-8"):
     return hashlib.sha3_256(string.encode(encode)).hexdigest()
 
-def sha3_384(string,encode='utf-8'):
+
+def sha3_384(string, encode="utf-8"):
     return hashlib.sha3_384(string.encode(encode)).hexdigest()
 
-def sha3_512(string,encode='utf-8'):
+
+def sha3_512(string, encode="utf-8"):
     return hashlib.sha3_512(string.encode(encode)).hexdigest()
 
-def blake2s(string,encode='utf-8'):
+
+def blake2s(string, encode="utf-8"):
     return hashlib.blake2s(string.encode(encode)).hexdigest()
 
-def blake2b(string,encode='utf-8'):
+
+def blake2b(string, encode="utf-8"):
     return hashlib.blake2b(string.encode(encode)).hexdigest()
 
-def shake_128(string,encode='utf-8'):
+
+def shake_128(string, encode="utf-8"):
     return hashlib.shake_128(string.encode(encode)).hexdigest(16)
 
-def shake_256(string,encode='utf-8'):
+
+def shake_256(string, encode="utf-8"):
     return hashlib.shake_256(string.encode(encode)).hexdigest(16)
 
-hash_types={'md5':md5,'sha1':sha1,'sha224':sha224,'sha256':shake_256,'sha384':sha384,'sha512':sha512,
-            'sha3_224':sha3_224,'sha3_256':sha3_256,'sha3_384':sha3_384,'sha3_512':sha3_512,
-            'blake2s':blake2s,'black2b':blake2b,'shake_128':shake_128,'shake_256':shake_256}
 
-def hash_file(directory,hash_type):
+def hash_file(directory, hash_type):
     f = open(directory)
-    fileStr =f.read()
-    encode=f.encoding
+    h=hashlib.new(hash_type)
+    while True:
+        data = f.read(2**20)
+        if not data:
+            break
+        data=data.encode(f.encoding)
+        h.update(data)
     f.close()
-    return hash_types[hash_type](fileStr,encode)
+    if hash_type.split('_')[0]=='shake':
+        return h.hexdigest(16)
+    else:
+        return h.hexdigest()
 
-def hash_directory(root_dir,hash_type):
-    hashes_list=[]
+
+def hash_directory(root_dir, hash_type):
+    hashes_list = []
     for d in get_list_files(root_dir):
-        hashes_list.append(hash_file(d,hash_type))
+        hashes_list.append(hash_file(d, hash_type))
     return hashes_list
