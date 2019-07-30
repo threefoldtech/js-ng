65cd8abc8dbbb091077c8d864a76f80ee6c4a7f0
omarabdul3ziz <omarabdul3ziz@gmail.com>
2019-07-30 18:21:56 +0200

    use with statement and add docstrings

--- jumpscale/sals/host_files/host_files.py
+++ jumpscale/sals/host_files/host_files.py
@@ -1,49 +1,48 @@
 import re
 
-hosts_file = "hosts.txt"
 
-
-def exists(ip, hosts_file):
-    file = open(hosts_file, "r")
-    file_content = file.read()
-    file.close()
-    return re.search(f"{ip}", file_content)
+def exists(ip, hosts_file="/etc/hosts"):
+    """check for the existence of the ip in hosts file and return boolen"""
+    with open(hosts_file, "r") as file:
+        file_content = file.read()
+    result = re.search(f"{ip}", file_content)
+    if result:
+        return True
+    else:
+        return None
 
 
 def remove(ip, hosts_file="/etc/hosts"):
-    file = open(hosts_file, "r")
-    lines = file.readlines()
+    """remove the ip and it's hostname from hosts file"""
+    with open(hosts_file, "r") as file:
+        lines = file.readlines()
     for line in lines:
         if line.startswith(f"{ip}"):
             lines.remove(line)
-    file.close()
-    file = open(hosts_file, "w")
-    file.write("".join(lines))
-    file.close
+    with open(hosts_file, "w") as file:
+        file.write("".join(lines))
 
 
-def add(ip, domain, hosts_file):
-    file = open(hosts_file, "a")
-    file.write(f"\n{ip}\t{domain}")
-    file.close()
+def add(ip, domain, hosts_file="/etc/hosts"):
+    """add new entry to the database"""
+    with open(hosts_file, "a") as file:
+        file.write(f"\n{ip}\t{domain}")
 
 
-def set_hostname(ip, domain, hosts_file):
+def set_hostname(ip, domain, hosts_file="/etc/hosts"):
+    """update the hostname for ip"""
     remove(ip, hosts_file)
     add(ip, domain, hosts_file)
 
 
-def get_hostname(ip, hosts_file):
+def get_hostname(ip, hosts_file="/etc/hosts"):
+    """get the the hostname for ip"""
     if exists(ip, hosts_file):
-        file = open(hosts_file, "r")
-        lines = file.readlines()
+        with open(hosts_file, "r") as file:
+            lines = file.readlines()
         for line in lines:
             if re.search(f"{ip}", line):
                 hostname = line.split("\t")[1]
         return hostname
     else:
         return "ip does not exist"
-
-
-print(get_hostname("159.0.0.0", hosts_file))
-
