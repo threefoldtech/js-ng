f9cc7584af333e6f5386d166d8c744db69f5aa0b
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-08-02 05:12:34 +0200

    experiments with base classes (factories)

---
+++ tests/test_factory.py
@@ -0,0 +1,41 @@
+import unittest
+
+from jumpscale.core.base import Base, Factory, DuplicateError
+
+
+class Address(Base):
+    def __init__(self):
+        super().__init__()
+        self.x = 123
+
+
+class Wallet(Base):
+    addresses = Factory(Address)
+
+
+class Client(Base):
+    wallets = Factory(Wallet)
+
+
+class TestBaseFactory(unittest.TestCase):
+    def test_create_with_different_instances(self):
+        cl = Client()
+        w = cl.wallets.get("aa")
+        self.assertEqual(cl.wallets.count(), 1)
+
+        addr1 = w.addresses.new("mine")
+        addr1.x = 456
+        addr2 = w.addresses.new("another")
+        addr2.x = 680
+        self.assertEqual(w.addresses.count(), 2)
+        w.addresses.delete("another")
+        self.assertEqual(w.addresses.count(), 1)
+
+        # test duplicates
+        with self.assertRaises(DuplicateError):
+            w.addresses.new("mine")
+
+        # create another instance
+        new_cl = Client()
+        self.assertEqual(new_cl.wallets.count(), 0)
+
