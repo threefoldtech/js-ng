263b9bb17422ba9b0cb574638cd6baa65aa74823
omarelsheekh <31706237+omarelsheekh@users.noreply.github.com>
2019-08-04 13:05:57 +0200

    add toml serializer

--- jumpscale/data/serializers/serializers.py
+++ jumpscale/data/serializers/serializers.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import toml
 
 
 def load(string,type):
@@ -17,8 +18,10 @@ def load(string,type):
     """
     if type=='yaml':
         return load_yaml(string)
+    elif type=='toml':
+        return load_toml(string)
     else:
-        raise Exception ('the type must be yaml or...')
+        raise Exception ('the type must be yaml,toml or')
 
 def dump(dict,type):
     """convert dict to formated string
@@ -35,8 +38,10 @@ def dump(dict,type):
     """
     if type=='yaml':
         return dump_yaml(dict)
+    elif type=='toml':
+        return dump_toml(dict)
     else:
-        raise Exception ('the type must be yaml or...')
+        raise Exception ('the type must be yaml,toml or')
 
 def load_yaml(string):
     """convert yaml string to dict
@@ -62,3 +67,26 @@ def dump_yaml(dict):
     y=yaml.safe_dump(dict)
     return y
 
+def load_toml(string):
+    """convert toml string to dict
+    
+    Arguments:
+        string {str} -- the toml string
+    
+    Returns:
+        dict -- the converted dict
+    """
+    d=toml.loads(string)
+    return d
+
+def dump_toml(dict):
+    """convert dict to toml string
+    
+    Arguments:
+        dict {dict} -- the dict will be converted
+    
+    Returns:
+        str -- the toml string generated from dict
+    """
+    s=toml.dumps(dict)
+    return s
--- tests/data/serializers/test_serializers.py
+++ tests/data/serializers/test_serializers.py
@@ -32,3 +32,46 @@ Table:
 'Paragraph': {'font': {'name': 'Times New Roman', 'size': 11, 'color_theme': 'ACCENT_2'}}, 'Table': {'style': 'MediumGrid3-Accent2'}}
     assert type(make_serializer.dump(yamdict,'yaml'))==type('just string')
     assert type(make_serializer.load(yamstr,'yaml'))==type({'dict':'test'})
+
+def test_toml(make_serializer):
+    tomstr = """
+# This is a TOML document.
+
+ title = "TOML Example"
+
+ [owner]
+ name = "Tom Preston-Werner"
+ dob = 1979-05-27T07:32:00-08:00 # First class dates
+
+ [database]
+ server = "192.168.1.1"
+ ports = [ 8001, 8001, 8002 ]
+ connection_max = 5000
+ enabled = true
+
+ [servers]
+
+   # Indentation (tabs and/or spaces) is allowed but not required
+   [servers.alpha]
+   ip = "10.0.0.1"
+   dc = "eqdc10"
+
+   [servers.beta]
+   ip = "10.0.0.2"
+   dc = "eqdc10"
+
+ [clients]
+ data = [ ["gamma", "delta"], [1, 2] ]
+
+ # Line breaks are OK when inside arrays
+ hosts = [
+   "alpha",
+   "omega"
+ ]
+ """
+    tomdict={'title': 'TOML Example', 'owner': {'name': 'Tom Preston-Werner',
+     'dob':'none','database': {'server': '192.168.1.1', 'ports': [8001, 8001, 8002], 'connection_max': 5000, 'enabled': True},
+     'servers': {'alpha': {'ip': '10.0.0.1', 'dc': 'eqdc10'}, 'beta': {'ip': '10.0.0.2', 'dc': 'eqdc10'}},
+      'clients': {'data': [['gamma', 'delta'], [1, 2]], 'hosts': ['alpha', 'omega']}}}
+    assert type(make_serializer.load(tomstr,'toml'))==type({'a':'b'})
+    assert type(make_serializer.dump(tomdict,'toml'))==type("str")
