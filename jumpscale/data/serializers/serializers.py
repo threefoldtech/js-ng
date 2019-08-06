1218fb1c68e1479e2398568f0787b4cd06b987d6
Omar Elsheekh <omarashraf0109@gmail.com>
2019-08-06 11:58:58 +0200

    test enviroment

--- jumpscale/data/serializers/serializers.py
+++ jumpscale/data/serializers/serializers.py
@@ -4,7 +4,8 @@ import toml
 import base64
 
 
-def load(string,type):
+# test ubuntu staff
+def load(string, type):
     """convert any formater to dict
     
     Arguments:
@@ -17,18 +18,19 @@ def load(string,type):
     Returns:
         dict -- the converted dict 
     """
-    if type=='yaml':
+    if type == "yaml":
         return load_yaml(string)
-    elif type=='toml':
+    elif type == "toml":
         return load_toml(string)
-    elif type=='json':
+    elif type == "json":
         return load_json(string)
-    elif type=='base64':
+    elif type == "base64":
         return load_base64(string)
     else:
-        raise Exception ('the type must be yaml,toml,json, or base64')
+        raise Exception("the type must be yaml,toml,json, or base64")
 
-def dump(dict,type):
+
+def dump(dict, type):
     """convert dict to formated string
     
     Arguments:
@@ -41,16 +43,17 @@ def dump(dict,type):
     Returns:
         str -- the formated string
     """
-    if type=='yaml':
+    if type == "yaml":
         return dump_yaml(dict)
-    elif type=='toml':
+    elif type == "toml":
         return dump_toml(dict)
-    elif type=='json':
+    elif type == "json":
         return dump_json(dict)
-    elif type=='base64':
+    elif type == "base64":
         return dump_base64(dict)
     else:
-        raise Exception ('the type must be yaml,toml,dict, or base')
+        raise Exception("the type must be yaml,toml,dict, or base")
+
 
 def load_yaml(string):
     """convert yaml string to dict
@@ -61,8 +64,9 @@ def load_yaml(string):
     Returns:
         dict -- the converted dict
     """
-    d=yaml.safe_load(string)
+    d = yaml.safe_load(string)
     return d
+
 
 def dump_yaml(dict):
     """convert dict to yaml string
@@ -73,8 +77,9 @@ def dump_yaml(dict):
     Returns:
         str -- the yaml string generated from dict
     """
-    y=yaml.safe_dump(dict)
+    y = yaml.safe_dump(dict)
     return y
+
 
 def load_toml(string):
     """convert toml string to dict
@@ -85,8 +90,9 @@ def load_toml(string):
     Returns:
         dict -- the converted dict
     """
-    d=toml.loads(string)
+    d = toml.loads(string)
     return d
+
 
 def dump_toml(dict):
     """convert dict to toml string
@@ -97,8 +103,9 @@ def dump_toml(dict):
     Returns:
         str -- the toml string generated from dict
     """
-    s=toml.dumps(dict)
+    s = toml.dumps(dict)
     return s
+
 
 def load_json(string):
     """convert json string to dict
@@ -109,8 +116,9 @@ def load_json(string):
     Returns:
         dict -- the converted dict
     """
-    d=json.loads(string.replace('\'','\"'))
+    d = json.loads(string.replace("'", '"'))
     return d
+
 
 def dump_json(dict):
     """convert dict to json string
@@ -121,8 +129,9 @@ def dump_json(dict):
     Returns:
         str -- the json string generated from dict
     """
-    s=json.dumps(dict)
+    s = json.dumps(dict)
     return s
+
 
 def load_base64(data):
     """convert base string or bytes to dict
@@ -133,12 +142,13 @@ def load_base64(data):
     Returns:
         dict -- the converted dict
     """
-    d={}
-    if isinstance(data,bytes):
-        d={'bytes':data,'string':base64.decodebytes(data)}
+    d = {}
+    if isinstance(data, bytes):
+        d = {"bytes": data, "string": base64.decodebytes(data)}
     else:
-        d={'bytes':base64.encodebytes(data.encode()),'string':data}
+        d = {"bytes": base64.encodebytes(data.encode()), "string": data}
     return d
+
 
 def dump_base64(dict):
     """convert dict to base64 bytes
@@ -149,5 +159,5 @@ def dump_base64(dict):
     Returns:
         byte -- the json bytes in dict
     """
-    return dict['bytes']
+    return dict["bytes"]
 
