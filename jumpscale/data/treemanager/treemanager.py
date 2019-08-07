3a43c099dc0a3662a36e77cdc5957d2a73011bb6
Omar Elawady <omarelawady1998@gmail.com>
2019-08-07 09:51:03 +0200

    Create stub

---
+++ jumpscale/data/treemanager/treemanager.py
@@ -0,0 +1,50 @@
+class TreeNode:
+    def __init__(self, name, parent, data=None):
+        pass
+
+    def children(self):
+        pass
+
+    def append_child(self, node):
+        pass
+
+    def search_by_name(self, name):
+        pass
+
+    def search_by_val(self, val):
+        pass
+
+    def remove_child(self, node):
+        pass
+
+    def remove_child_by_name(self, name):
+        pass
+
+    def get_data(self):
+        pass
+
+    def get_parent(self):
+        pass
+
+
+class Tree:
+    def __init__(self):
+        pass
+
+    def search_by_value(self, value):
+        pass
+
+    def search_by_name(self, nane):
+        pass
+
+    def get_by_path(self, path):
+        pass
+
+    def delete_node(self, node):
+        pass
+
+    def add_node_by_path(self, path, name, data=None):
+        pass
+
+    def __str__(self):
+        pass
