f4e527c9e77416cf5726036b4f129657c2925f12
Omar Elsheekh <omarashraf0109@gmail.com>
2019-08-07 14:19:38 +0200

    bug fix

--- tests/data/hash/test_hashtool.py
+++ tests/data/hash/test_hashtool.py
@@ -5,12 +5,6 @@ import os
 
 
 @pytest.fixture
-def make_hash():
-    h = j.data.hash
-    return h
-
-
-@pytest.fixture
 def make_list():
     hashes_list = [
         "md5",
@@ -29,42 +23,42 @@ def make_list():
     return hashes_list
 
     
-def test_hash_strings(make_hash):
+def test_hash_strings():
     test_string = "my company is codescalers"
-    assert make_hash.md5(test_string).hexdigest() == hashlib.md5(test_string.encode()).hexdigest()
-    assert make_hash.sha1(test_string).hexdigest() == hashlib.sha1(test_string.encode()).hexdigest()
-    assert make_hash.sha224(test_string).hexdigest() == hashlib.sha224(test_string.encode()).hexdigest()
-    assert make_hash.sha256(test_string).hexdigest() == hashlib.sha256(test_string.encode()).hexdigest()
-    assert make_hash.sha384(test_string).hexdigest() == hashlib.sha384(test_string.encode()).hexdigest()
-    assert make_hash.sha512(test_string).hexdigest() == hashlib.sha512(test_string.encode()).hexdigest()
-    assert make_hash.sha3_224(test_string).hexdigest() == hashlib.sha3_224(test_string.encode()).hexdigest()
-    assert make_hash.sha3_256(test_string).hexdigest() == hashlib.sha3_256(test_string.encode()).hexdigest()
-    assert make_hash.sha3_384(test_string).hexdigest() == hashlib.sha3_384(test_string.encode()).hexdigest()
-    assert make_hash.sha3_512(test_string).hexdigest() == hashlib.sha3_512(test_string.encode()).hexdigest()
-    assert make_hash.blake2b(test_string).hexdigest() == hashlib.blake2b(test_string.encode()).hexdigest()
-    assert make_hash.blake2s(test_string).hexdigest() == hashlib.blake2s(test_string.encode()).hexdigest()
-    assert make_hash.shake_128(test_string).hexdigest(16) == hashlib.shake_128(test_string.encode()).hexdigest(16)
-    assert make_hash.shake_256(test_string).hexdigest(16) == hashlib.shake_256(test_string.encode()).hexdigest(16)
+    assert j.data.hash.md5(test_string).hexdigest() == hashlib.md5(test_string.encode()).hexdigest()
+    assert j.data.hash.sha1(test_string).hexdigest() == hashlib.sha1(test_string.encode()).hexdigest()
+    assert j.data.hash.sha224(test_string).hexdigest() == hashlib.sha224(test_string.encode()).hexdigest()
+    assert j.data.hash.sha256(test_string).hexdigest() == hashlib.sha256(test_string.encode()).hexdigest()
+    assert j.data.hash.sha384(test_string).hexdigest() == hashlib.sha384(test_string.encode()).hexdigest()
+    assert j.data.hash.sha512(test_string).hexdigest() == hashlib.sha512(test_string.encode()).hexdigest()
+    assert j.data.hash.sha3_224(test_string).hexdigest() == hashlib.sha3_224(test_string.encode()).hexdigest()
+    assert j.data.hash.sha3_256(test_string).hexdigest() == hashlib.sha3_256(test_string.encode()).hexdigest()
+    assert j.data.hash.sha3_384(test_string).hexdigest() == hashlib.sha3_384(test_string.encode()).hexdigest()
+    assert j.data.hash.sha3_512(test_string).hexdigest() == hashlib.sha3_512(test_string.encode()).hexdigest()
+    assert j.data.hash.blake2b(test_string).hexdigest() == hashlib.blake2b(test_string.encode()).hexdigest()
+    assert j.data.hash.blake2s(test_string).hexdigest() == hashlib.blake2s(test_string.encode()).hexdigest()
+    assert j.data.hash.shake_128(test_string).hexdigest(16) == hashlib.shake_128(test_string.encode()).hexdigest(16)
+    assert j.data.hash.shake_256(test_string).hexdigest(16) == hashlib.shake_256(test_string.encode()).hexdigest(16)
 
-def test_hash_bytes(make_hash):
+def test_hash_bytes():
     test_string = b'hi there'
-    assert make_hash.md5(test_string).hexdigest() == hashlib.md5(test_string).hexdigest()
-    assert make_hash.sha1(test_string).hexdigest() == hashlib.sha1(test_string).hexdigest()
-    assert make_hash.sha224(test_string).hexdigest() == hashlib.sha224(test_string).hexdigest()
-    assert make_hash.sha256(test_string).hexdigest() == hashlib.sha256(test_string).hexdigest()
-    assert make_hash.sha384(test_string).hexdigest() == hashlib.sha384(test_string).hexdigest()
-    assert make_hash.sha512(test_string).hexdigest() == hashlib.sha512(test_string).hexdigest()
-    assert make_hash.sha3_224(test_string).hexdigest() == hashlib.sha3_224(test_string).hexdigest()
-    assert make_hash.sha3_256(test_string).hexdigest() == hashlib.sha3_256(test_string).hexdigest()
-    assert make_hash.sha3_384(test_string).hexdigest() == hashlib.sha3_384(test_string).hexdigest()
-    assert make_hash.sha3_512(test_string).hexdigest() == hashlib.sha3_512(test_string).hexdigest()
-    assert make_hash.blake2b(test_string).hexdigest() == hashlib.blake2b(test_string).hexdigest()
-    assert make_hash.blake2s(test_string).hexdigest() == hashlib.blake2s(test_string).hexdigest()
-    assert make_hash.shake_128(test_string).hexdigest(16) == hashlib.shake_128(test_string).hexdigest(16)
-    assert make_hash.shake_256(test_string).hexdigest(16) == hashlib.shake_256(test_string).hexdigest(16)
+    assert j.data.hash.md5(test_string).hexdigest() == hashlib.md5(test_string).hexdigest()
+    assert j.data.hash.sha1(test_string).hexdigest() == hashlib.sha1(test_string).hexdigest()
+    assert j.data.hash.sha224(test_string).hexdigest() == hashlib.sha224(test_string).hexdigest()
+    assert j.data.hash.sha256(test_string).hexdigest() == hashlib.sha256(test_string).hexdigest()
+    assert j.data.hash.sha384(test_string).hexdigest() == hashlib.sha384(test_string).hexdigest()
+    assert j.data.hash.sha512(test_string).hexdigest() == hashlib.sha512(test_string).hexdigest()
+    assert j.data.hash.sha3_224(test_string).hexdigest() == hashlib.sha3_224(test_string).hexdigest()
+    assert j.data.hash.sha3_256(test_string).hexdigest() == hashlib.sha3_256(test_string).hexdigest()
+    assert j.data.hash.sha3_384(test_string).hexdigest() == hashlib.sha3_384(test_string).hexdigest()
+    assert j.data.hash.sha3_512(test_string).hexdigest() == hashlib.sha3_512(test_string).hexdigest()
+    assert j.data.hash.blake2b(test_string).hexdigest() == hashlib.blake2b(test_string).hexdigest()
+    assert j.data.hash.blake2s(test_string).hexdigest() == hashlib.blake2s(test_string).hexdigest()
+    assert j.data.hash.shake_128(test_string).hexdigest(16) == hashlib.shake_128(test_string).hexdigest(16)
+    assert j.data.hash.shake_256(test_string).hexdigest(16) == hashlib.shake_256(test_string).hexdigest(16)
 
 
-def test_hash_files(make_hash, make_list):
+def test_hash_files(make_list):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     os.path.join(THIS_FOLDER, "copy.txt")
     file = open("copy.txt", "w")
@@ -73,11 +67,11 @@ def test_hash_files(make_hash, make_list):
     for h in make_list:
         ha = hashlib.new(h)
         ha.update("Your text goes here".encode())
-        assert make_hash.hash_file("copy.txt", h).hexdigest() == ha.hexdigest()
+        assert j.data.hash.hash_file("copy.txt", h).hexdigest() == ha.hexdigest()
     os.remove('copy.txt')
 
 
-def test_hash_dir(make_hash, make_list):
+def test_hash_dir(make_list):
     cdir=os.getcwd()
     os.mkdir(cdir+'/omar')
     os.path.join(cdir+'/omar','hi.txt')
@@ -87,6 +81,6 @@ def test_hash_dir(make_hash, make_list):
     for h in make_list:
         ha = hashlib.new(h)
         ha.update("hi there".encode())
-        assert make_hash.hash_directory(cdir+'/omar',h)[0].hexdigest() == ha.hexdigest()
+        assert j.data.hash.hash_directory(cdir+'/omar',h)[0].hexdigest() == ha.hexdigest()
     os.remove(cdir+'/omar/hi.txt')
     os.rmdir(cdir+'/omar')
