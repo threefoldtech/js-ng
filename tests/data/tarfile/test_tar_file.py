fa1bba7e7e5f2abbdaa1826b308ecfd110a11878
Omar <omarabdul3ziz@gmail.com>
2019-08-07 16:47:42 +0200

    on progress

--- tests/data/tarfile/test_tar_file.py
+++ tests/data/tarfile/test_tar_file.py
@@ -4,18 +4,16 @@ import os.path
 from jumpscale.god import j
 
 
-# def test_istar():
-#     sample = tempfile.NamedTemporaryFile()
-#     j.data.tarfile.archive(sample.name, "/tmp/z.tar.gz")
-#     assert j.data.tarfile.istar("/tmp/z.tar.gz")
-
-
 # def test_archive():
 #     sample = tempfile.NamedTemporaryFile()
-#     j.data.tarfile.archive(sample.name, "/tmp/x.gz")
-#     j.data.tarfile.archive(sample.name, "/tmp/y.gz")
-#     assert os.path.isfile("/tmp/x.gz")
-#     assert os.path.isfile("/tmp/y.gz")
+#     j.data.tarfile.archive(sample.name, "/tmp/x")
+#     assert os.path.isfile("/tmp/x")
+
+
+def test_istar():
+    sample = tempfile.NamedTemporaryFile()
+    j.data.tarfile.archive(sample.name, "/tmp/z")
+    assert j.data.tarfile.istar("/tmp/z")
 
 
 # def test_get_content():
@@ -25,12 +23,12 @@ from jumpscale.god import j
 #     assert tmp.name.lstrip("/") in content
 
 
-def test_extrect():
-    sample = tempfile.NamedTemporaryFile()
-    j.data.tarfile.archive(sample.name, "/tmp/xy.gz")
-    with j.data.tarfile.TarReader("/tmp/xy.gz") as f:
-        f.extract("/tmp/xy")
-        assert os.path.isdir("/tmp/xy")
+# def test_extrect():
+#     sample = tempfile.NamedTemporaryFile()
+#     j.data.tarfile.archive(sample.name, "/tmp/xy.gz")
+#     with j.data.tarfile.TarReader("/tmp/xy.gz") as f:
+#         f.extract("/tmp/xy")
+#         assert os.path.isdir("/tmp/xy")
 
 
 # def test_extract_file():
