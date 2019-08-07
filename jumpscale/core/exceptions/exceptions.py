65493065845ec638450642ee40287252b50b2a83
Omar Elawady <omarelawady1998@gmail.com>
2019-08-07 16:13:49 +0200

    Add category and level default values

--- jumpscale/core/exceptions/exceptions.py
+++ jumpscale/core/exceptions/exceptions.py
@@ -1,28 +1,35 @@
 class JSException(Exception):
-    def __init__(self, message, category, level, context=None):
+    def __init__(self, message, category=None, level=None, context=None):
         super().__init__(message)
 
 
 class Base(JSException):
     pass
 
+
 class Permission(JSException):
     pass
+
 
 class Halt(JSException):
     pass
 
+
 class Runtime(JSException):
     pass
+
 
 class Input(JSException):
     pass
 
+
 class Value(JSException):
     pass
 
+
 class NotImplemented(JSException):
     pass
+
 
 class Bug(JSException):
     pass
@@ -31,19 +38,23 @@ class Bug(JSException):
 class Operations(JSException):
     pass
 
+
 class IO(JSException):
     pass
+
 
 class NotFound(JSException):
     pass
 
+
 class Timeout(JSException):
     pass
+
 
 class SSH(JSException):
     pass
 
+
 class SSHTimeout(JSException):
     pass
-
 
