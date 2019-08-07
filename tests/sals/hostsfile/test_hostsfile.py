efbe1c89c34fb920bcd58a4936557de18cde398b
Omar <omarabdul3ziz@gmail.com>
2019-08-07 18:58:51 +0200

    edit tests

--- tests/sals/hostsfile/test_hostsfile.py
+++ tests/sals/hostsfile/test_hostsfile.py
@@ -1,35 +1,42 @@
 import tempfile
 from jumpscale.god import j
 
-tmp = tempfile.NamedTemporaryFile(suffix=".txt")
-hf = j.sals.hostsfile.HostsFile(tmp.name)
-
 
 def test_add():
-    hf.add("0.0.0.0", "a")
-    hf.add("0.0.0.1", "b")
-    hf.add("0.0.0.2", "c")
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tmp:
+        hf = j.sals.hostsfile.HostsFile(tmp.name)
+        hf.add("0.0.0.0", "a")
+        hf.add("0.0.0.1", "b")
+        hf.add("0.0.0.2", "c")
     assert "0.0.0.0" in hf.content
     assert "0.0.0.1" in hf.content
     assert "0.0.0.2" in hf.content
 
 
 def test_exists():
-    hf.add("0.0.1.0", "d")
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tmp:
+        hf = j.sals.hostsfile.HostsFile(tmp.name)
+        hf.add("0.0.1.0", "d")
     assert hf.exists("0.0.1.0") == True
     assert hf.exists("0.0.1.1") == False
 
 
 def test_remove():
-    hf.add("0.0.2.0", "e")
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tmp:
+        hf = j.sals.hostsfile.HostsFile(tmp.name)
+        hf.add("0.0.2.0", "e")
     assert hf.remove("0.0.2.0") not in hf.content
 
 
 def test_set_hostname():
-    hf.set_hostname("0.0.3.0", "f")
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tmp:
+        hf = j.sals.hostsfile.HostsFile(tmp.name)
+        hf.set_hostname("0.0.3.0", "f")
     assert "0.0.3.0" in hf.content
 
 
 def test_get_hostname():
-    hf.add("0.0.4.0", "h")
+    with tempfile.NamedTemporaryFile(suffix=".txt") as tmp:
+        hf = j.sals.hostsfile.HostsFile(tmp.name)
+        hf.add("0.0.4.0", "h")
     assert hf.get_hostname("0.0.4.0") == "h"
