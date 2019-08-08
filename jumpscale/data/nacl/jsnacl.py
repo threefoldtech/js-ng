8cb8fff33d7f034f247ff4376d077bec768d66f1
Omar Elawady <omarelawady1998@gmail.com>
2019-08-08 14:40:15 +0200

    Implement nacl as a class

--- jumpscale/data/nacl/jsnacl.py
+++ jumpscale/data/nacl/jsnacl.py
@@ -4,50 +4,43 @@ from nacl.public import PrivateKey, PublicKey, Box
 from nacl.secret import SecretBox
 from nacl.signing import SigningKey, VerifyKey
 
-KEY_SIZE = 32
 
+class NACL:
+    KEY_SIZE = 32
 
-def generate_random_key(size=KEY_SIZE):
-    return nacl.utils.random(size)
+    def __init__(self, private_key=None, symmetric_key=None, signing_seed=None):
+        self.private_key = PrivateKey.generate() if private_key is None else PrivateKey(private_key)
+        self.public_key = self.private_key.public_key
+        self.symmetric_key = nacl.utils.random(KEY_SIZE) if symmetric_key is None else symmetric_key
+        self.signing_key = SigningKey.generate() if signing_seed is None else SigningKey(signing_seed)
+        self.symmetric_box = SecretBox(self.symmetric_key)
 
+    def encrypt(self, message, reciever_public_key):
+        return Box(self.private_key, PublicKey(reciever_public_key)).encrypt(message)
 
-def asymmetric_box(private_key, public_key):
-    return Box(PrivateKey(private_key), PublicKey(public_key))
+    def decrypt(self, message, sender_public_key):
+        return Box(self.private_key, PublicKey(sender_public_key)).decode(message)
 
+    def encrypt_symmetric(self, message):
+        return self.symmetric_box.encrypt(message)
 
-def public_from_private(private_key):
-    return bytes(PrivateKey(private_key).public_key)
+    def sign(self, message):
+        return self.signing_key.sign(message)
 
+    def verify(self, message, signature, verification_key):
+        return VerifyKey(verification_key).verify(message, signature)
 
-def verifaction_from_signing(seed):
-    return bytes(SigningKey(seed).verify_key)
+    def get_signing_seed(self):
+        return bytes(self.signing_key._seed)
 
+    def get_verification_key(self):
+        return bytes(self.signing_key.verify_key)
 
-def symmetric_box(key):
-    return SecretBox(key)
+    def get_public_key(self):
+        return bytes(self.public_key)
 
+    def get_private_key(self):
+        return bytes(self.private_key)
 
-def encrypt_asymmetric(message, box):
-    return box.encrypt(message)
-
-
-def decrypt_asymmetric(message, box):
-    return box.decrypt(message)
-
-
-def encrypt_symmetric(message, box):
-    return box.encrypt(message)
-
-
-def decrypt_symmetric(message, box):
-    return box.decrypt(message)
-
-
-def sign(message, seed):
-    signing_key = SigningKey(seed)
-    signed = signing_key.sign(message)
-    return signed.message, signed.signature
-
-
-def verify(message, signature, verifaction_key):
-    return VerifyKey(verifaction_key).verify(message, signature)
+    def get_symmetric_key(self):
+        return bytes(self.symmetric_key)
