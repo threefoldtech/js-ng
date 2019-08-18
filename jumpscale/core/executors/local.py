c8ac1aa996c491860731157b3b3462908ef06d3e
xmonader <xmonader@gmail.com>
2019-08-18 14:20:04 +0200

    update executors local and remote

--- jumpscale/core/executors/local.py
+++ jumpscale/core/executors/local.py
@@ -1,6 +1,144 @@
 import invoke
+"""
+JS-NG> j.core.executors.run_local("echo $WHOME", env={'WHOME':'abc'}, ech
+     1 o=True)                                                           
+echo $WHOME
+abc
+<Result cmd='echo $WHOME' exited=0>
 
-def execute(cmd, command_ctx=None, connection_ctx=None):
+JS-NG> j.core.executors.run_local("uname", echo=True)                    
+uname
+Linux
+<Result cmd='uname' exited=0>
+
+JS-NG> j.core.executors.run_local("uname", echo=False)                   
+Linux
+<Result cmd='uname' exited=0>
+
+"""
+
+
+def execute(cmd, **command_ctx):
+    """
+    :param str shell:
+        Which shell binary to use. Default: ``/bin/bash`` (on Unix;
+        ``COMSPEC`` or ``cmd.exe`` on Windows.)
+    :param bool warn:
+        Whether to warn and continue, instead of raising
+        `.UnexpectedExit`, when the executed command exits with a
+        nonzero status. Default: ``False``.
+        .. note::
+            This setting has no effect on exceptions, which will still be
+            raised, typically bundled in `.ThreadException` objects if they
+            were raised by the IO worker threads.
+            Similarly, `.WatcherError` exceptions raised by
+            `.StreamWatcher` instances will also ignore this setting, and
+            will usually be bundled inside `.Failure` objects (in order to
+            preserve the execution context).
+            Ditto `.CommandTimedOut` - basically, anything that prevents a
+            command from actually getting to "exited with an exit code"
+            ignores this flag.
+    :param hide:
+        Allows the caller to disable ``run``'s default behavior of copying
+        the subprocess' stdout and stderr to the controlling terminal.
+        Specify ``hide='out'`` (or ``'stdout'``) to hide only the stdout
+        stream, ``hide='err'`` (or ``'stderr'``) to hide only stderr, or
+        ``hide='both'`` (or ``True``) to hide both streams.
+        The default value is ``None``, meaning to print everything;
+        ``False`` will also disable hiding.
+        .. note::
+            Stdout and stderr are always captured and stored in the
+            ``Result`` object, regardless of ``hide``'s value.
+        .. note::
+            ``hide=True`` will also override ``echo=True`` if both are
+            given (either as kwargs or via config/CLI).
+    :param bool pty:
+        By default, ``run`` connects directly to the invoked process and
+        reads its stdout/stderr streams. Some programs will buffer (or even
+        behave) differently in this situation compared to using an actual
+        terminal or pseudoterminal (pty). To use a pty instead of the
+        default behavior, specify ``pty=True``.
+        .. warning::
+            Due to their nature, ptys have a single output stream, so the
+            ability to tell stdout apart from stderr is **not possible**
+            when ``pty=True``. As such, all output will appear on
+            ``out_stream`` (see below) and be captured into the ``stdout``
+            result attribute. ``err_stream`` and ``stderr`` will always be
+            empty when ``pty=True``.
+    :param bool fallback:
+        Controls auto-fallback behavior re: problems offering a pty when
+        ``pty=True``. Whether this has any effect depends on the specific
+        `Runner` subclass being invoked. Default: ``True``.
+    :param bool echo:
+        Controls whether `.run` prints the command string to local stdout
+        prior to executing it. Default: ``False``.
+        .. note::
+            ``hide=True`` will override ``echo=True`` if both are given.
+    :param dict env:
+        By default, subprocesses receive a copy of Invoke's own environment
+        (i.e. ``os.environ``). Supply a dict here to update that child
+        environment.
+        For example, ``run('command', env={'PYTHONPATH':
+        '/some/virtual/env/maybe'})`` would modify the ``PYTHONPATH`` env
+        var, with the rest of the child's env looking identical to the
+        parent.
+        .. seealso:: ``replace_env`` for changing 'update' to 'replace'.
+    :param bool replace_env:
+        When ``True``, causes the subprocess to receive the dictionary
+        given to ``env`` as its entire shell environment, instead of
+        updating a copy of ``os.environ`` (which is the default behavior).
+        Default: ``False``.
+    :param str encoding:
+        Override auto-detection of which encoding the subprocess is using
+        for its stdout/stderr streams (which defaults to the return value
+        of `default_encoding`).
+    :param out_stream:
+        A file-like stream object to which the subprocess' standard output
+        should be written. If ``None`` (the default), ``sys.stdout`` will
+        be used.
+    :param err_stream:
+        Same as ``out_stream``, except for standard error, and defaulting
+        to ``sys.stderr``.
+    :param in_stream:
+        A file-like stream object to used as the subprocess' standard
+        input. If ``None`` (the default), ``sys.stdin`` will be used.
+        If ``False``, will disable stdin mirroring entirely (though other
+        functionality which writes to the subprocess' stdin, such as
+        autoresponding, will still function.) Disabling stdin mirroring can
+        help when ``sys.stdin`` is a misbehaving non-stream object, such as
+        under test harnesses or headless command runners.
+    :param watchers:
+        A list of `.StreamWatcher` instances which will be used to scan the
+        program's ``stdout`` or ``stderr`` and may write into its ``stdin``
+        (typically ``str`` or ``bytes`` objects depending on Python
+        version) in response to patterns or other heuristics.
+        See :doc:`/concepts/watchers` for details on this functionality.
+        Default: ``[]``.
+    :param bool echo_stdin:
+        Whether to write data from ``in_stream`` back to ``out_stream``.
+        In other words, in normal interactive usage, this parameter
+        controls whether Invoke mirrors what you type back to your
+        terminal.
+        By default (when ``None``), this behavior is triggered by the
+        following:
+            * Not using a pty to run the subcommand (i.e. ``pty=False``),
+                as ptys natively echo stdin to stdout on their own;
+            * And when the controlling terminal of Invoke itself (as per
+                ``in_stream``) appears to be a valid terminal device or TTY.
+                (Specifically, when `~invoke.util.isatty` yields a ``True``
+                result when given ``in_stream``.)
+                .. note::
+                    This property tends to be ``False`` when piping another
+                    program's output into an Invoke session, or when running
+                    Invoke within another program (e.g. running Invoke from
+                    itself).
+        If both of those properties are true, echoing will occur; if either
+        is false, no echoing will be performed.
+        When not ``None``, this parameter will override that auto-detection
+        and force, or disable, echoing.
+    :param timeout:
+
+    """
     ## use formatter to format command
     command_ctx = command_ctx or {}
     return invoke.run(cmd, **command_ctx)
--- jumpscale/core/executors/remote.py
+++ jumpscale/core/executors/remote.py
@@ -1,5 +1,36 @@
 import fabric
 
+"""
+
+
+JS-NG> with j.core.executors.RemoteExecutor(host="local
+     1 host",     connect_kwargs={ 
+     2         "key_filename": "/home/xmonader/.ssh/id_
+...    rsa", 
+     3     },) as c: 
+     4     c.run("hostname")                           
+xmonader-ThinkPad-E580
+JS-NG>  
+
+
+
+
+"""
+
+class RemoteExecutor:
+    def __init__(self, **connection_ctx):
+        self._connection_ctx = connection_ctx
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        pass
+
+    def run(self, cmd, **command_ctx):
+        with fabric.Connection(**self._connection_ctx) as c:
+            return c.run(cmd, **command_ctx) 
+
 def execute(cmd, command_ctx, connection_ctx):
     """
     kwargs: fabric.Connection(host, user=None, port=None, config=None, gateway=None, forward_agent=None, connect_timeout=None, connect_kwargs=None, inline_ssh_env=None)
