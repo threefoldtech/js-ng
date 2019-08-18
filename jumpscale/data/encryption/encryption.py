1e409d20db5ee7b2a3645596623a96b6f9ec711c
Omar Elawady <omarelawady1998@gmail.com>
2019-08-18 13:27:58 +0200

    Add docstrings

--- jumpscale/data/encryption/encryption.py
+++ jumpscale/data/encryption/encryption.py
@@ -4,12 +4,32 @@ from .mnemonic import key_to_mnemonic, mnemonic_to_key, generate_mnemonic
 
 
 def encrypt(message, private_key, receiver_public_key):
+    """Encrypt the message using the public key of the reciever and the sender's private key.
+    
+    Args:
+        message (bytes): The message.
+        private_key (bytes): sender's private key.
+        receiver_public_key (bytes): receiver's public key.
+
+    Returns:
+        bytes: The encryption of the message.
+    """
     nacl_obj = j.data.nacl.NACL(private_key=private_key)
     message = nacl_obj.encrypt(message, receiver_public_key)
     return message
 
 
 def decypt(message, private_key, sender_public_key):
+    """Decrypt the message using the private key of the reciever and the sender's public key.
+    
+    Args:
+        message (bytes): The message.
+        private_key (bytes): reciever's private key.
+        sender_public_key (bytes): sender's public key.
+    
+    Returns:
+        bytes: The decryption of the message.
+    """
     nacl_obj = j.data.nacl.NACL(private_key=private_key)
     message = nacl_obj.decrypt(message, sender_public_key)
     return message
