1e4a94f9392a6c73c0f25fe99011b11bd99dec57
Omar Elawady <omarelawady1998@gmail.com>
2019-08-18 12:36:11 +0200

    Remove signing, import mnemonic functions.

--- jumpscale/data/encryption/encryption.py
+++ jumpscale/data/encryption/encryption.py
@@ -1,19 +1,15 @@
 from jumpscale.god import j
 from .exceptions import CoudntVerifyAuthorError
+from .mnemonic import key_to_mnemonic, mnemonic_to_key, generate_mnemonic
 
 
-def sign_encrypt(message, private_key, receiver_public_key):
-    nacl_obj = j.data.nacl.NACL(private_key=private_key, signing_seed=private_key)
-    message, signature = nacl_obj.sign(message)
+def encrypt(message, private_key, receiver_public_key):
+    nacl_obj = j.data.nacl.NACL(private_key=private_key)
     message = nacl_obj.encrypt(message, receiver_public_key)
-    signature = nacl_obj.encrypt(signature, receiver_public_key)
-    return message, signature
+    return message
 
 
-def decypt_verify(message, signature, private_key, sender_public_key, verification_key):
-    nacl_obj = j.data.nacl.NACL(private_key=private_key, signing_seed=private_key)
+def decypt(message, private_key, sender_public_key):
+    nacl_obj = j.data.nacl.NACL(private_key=private_key)
     message = nacl_obj.decrypt(message, sender_public_key)
-    signature = nacl_obj.decrypt(signature, sender_public_key)
-    if not nacl_obj.verify(message, signature, verification_key):
-        raise CoudntVerifyAuthorError("The sender of the message couldn't be verified")
     return message
