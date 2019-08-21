176df1cd3769b0cc8e21fcf7d959ad412c03bd02
Omar Elawady <omarelawady1998@gmail.com>
2019-08-21 13:37:30 +0200

    Add list and type inference.

--- jumpscale/data/types/pritypes.py
+++ jumpscale/data/types/pritypes.py
@@ -43,14 +43,14 @@ class Integer:
             return None
 
 
-class Boolen:
+class Boolean:
     def __init__(self, default=None):
         if not default:
             default = "False"
         self.default = self.from_str(default)
 
     def check(self, value):
-        """Check whether provided string represent boolen expresion
+        """Check whether provided string represent Boolean expresion
 
         Arguments:
             value (str)
@@ -59,7 +59,7 @@ class Boolen:
         return value in valid_expresions
 
     def from_str(self, value):
-        """Get the boolen value from the string
+        """Get the Boolean value from the string
 
         Arguments:
             value (str)
@@ -101,3 +101,50 @@ class Float:
         except ValueError:
             return None
 
+
+class JSObject:
+    def __init__(self, default=None):
+        default = default or ""
+        self.default = self.from_str(default)
+
+    def check(self, value):
+        return isinstance(value, str)
+
+    def from_str(self, value):
+        return value
+
+
+class List:
+    def __init__(self, default, subtype):
+        self.subtype = subtype(default)
+
+    def check(self, value):
+        valid = len(value) >= 2 and value[0] == "[" and value[-1] == "]"
+        if not valid:
+            return False
+        # TODO: elements with "," will fail
+        elems = value.split(",")
+        for e in elems:
+            e = e.strip()
+            if not self.subtype.check(e):
+                return False
+
+        return True
+
+    def from_str(self, value):
+        value = value[1:-1]
+        lst = []
+        elems = value.split(",")
+        for e in elems:
+            e = e.strip()
+            lst.append(self.subtype.from_str(e))
+        return lst
+
+
+def get_js_type(type_str, default_value):
+    types = {"": String, "S": String, "I": Integer, "bool": Boolean, "Float": Float, "O": JSObject}
+    if type_str[0] != "L":
+        return types[type_str](default_value)
+    else:
+        subtype = types[type_str[1:]]
+        return List(default_value, subtype)
