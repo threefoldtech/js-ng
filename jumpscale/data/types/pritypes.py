d55f420640dbe68ce38a1a5978eec404fe538819
Omar Elawady <omarelawady1998@gmail.com>
2019-08-21 17:26:58 +0200

    Implement string parser

--- jumpscale/data/types/pritypes.py
+++ jumpscale/data/types/pritypes.py
@@ -1,3 +1,6 @@
+import ast
+
+
 class String:
     def __init__(self, default=None):
         if not default:
@@ -116,35 +119,45 @@ class JSObject:
 
 class List:
     def __init__(self, default, subtype):
-        self.subtype = subtype(default)
+        self.subtype = subtype
+        default = default or "[]"
+        self.default = self.from_str(default)
+
+    def _deep_shick(self, value):
+        for e in value:
+            if isinstance(self.subtype, List) and not self.subtype._deep_shick(e):
+                return False
+            elif not isinstance(self.subtype, List) and not self.subtype.check(str(e)):
+                return False
+        return True
 
     def check(self, value):
         valid = len(value) >= 2 and value[0] == "[" and value[-1] == "]"
         if not valid:
             return False
-        # TODO: elements with "," will fail
-        elems = value.split(",")
-        for e in elems:
-            e = e.strip()
-            if not self.subtype.check(e):
-                return False
-
+        try:
+            return self._deep_shick(ast.literal_eval(value))
+        except:
+            return False
         return True
 
+    def _deep_parse(self, value):
+        if isinstance(self.subtype, List):
+            for i, e in enumerate(value):
+                value[i] = self.subtype._deep_parse(value)
+        else:
+            for i, e in enumerate(value):
+                value[i] = self.subtype.from_str(str(e))
+        return value
+
     def from_str(self, value):
-        value = value[1:-1]
-        lst = []
-        elems = value.split(",")
-        for e in elems:
-            e = e.strip()
-            lst.append(self.subtype.from_str(e))
-        return lst
+        return self._deep_parse(ast.literal_eval(value))
 
 
-def get_js_type(type_str, default_value):
-    types = {"": String, "S": String, "I": Integer, "bool": Boolean, "Float": Float, "O": JSObject}
-    if type_str[0] != "L":
+def get_js_type(type_str, default_value=None):
+    types = {"": String, "S": String, "I": Integer, "bool": Boolean, "F": Float, "O": JSObject}
+    if len(type_str) == 0 or type_str[0] != "L":
         return types[type_str](default_value)
     else:
-        subtype = types[type_str[1:]]
-        return List(default_value, subtype)
+        subtype = type_str[1:]
+        return List(default_value, get_js_type(subtype))
