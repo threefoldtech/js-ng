4d3f315ac81d15f68cf546323c0f2fcbf2506eb2
Omar Elawady <omarelawady1998@gmail.com>
2019-08-21 17:27:28 +0200

    Modify schema tests

--- tests/data/schema/test_schema.py
+++ tests/data/schema/test_schema.py
@@ -13,13 +13,13 @@ def test_schema():
     obj = (O)!7mada.test
     
     
-    date_start = 0 (D)
+    date_start = 0 (I)
     
        description* = ""        
     description2 ** = (S)
-    llist4*** = [1,2,3] (L)
+    llist4*** = [1,2,3] (LI)
     llist5 = [1,2,3] (LI)
-    llist6 = "1,2,3" (LI)
+    llist6 = [1,2,3] (LI)
     U = 0.0
     nrdefault = 0
     nrdefault2 = (I)
@@ -29,8 +29,13 @@ def test_schema():
     assert schema.url == "despiegk.test"
     assert schema.system_props == {"url": "despiegk.test"}
     assert "llist" in schema.props
+    assert (
+        isinstance(schema.props["llist"].type, j.data.types.List)
+        and isinstance(schema.props["llist"].type.subtype, j.data.types.String)
+        and schema.props["llist"].type.default == []
+    )
+    del schema.props["llist"].type
     assert schema.props["llist"].__dict__ == {
-        "type": "ls",
         "defaultvalue": "[]",
         "index": False,
         "index_key": False,
@@ -40,8 +45,13 @@ def test_schema():
         "name": "llist",
     }
     assert "llist2" in schema.props
+    assert (
+        isinstance(schema.props["llist2"].type, j.data.types.List)
+        and isinstance(schema.props["llist2"].type.subtype, j.data.types.String)
+        and schema.props["llist2"].type.default == []
+    )
+    del schema.props["llist2"].type
     assert schema.props["llist2"].__dict__ == {
-        "type": "LS",
         "defaultvalue": "",
         "index": False,
         "index_key": False,
@@ -51,8 +61,13 @@ def test_schema():
         "name": "llist2",
     }
     assert "llist3" in schema.props
+    assert (
+        isinstance(schema.props["llist3"].type, j.data.types.List)
+        and isinstance(schema.props["llist3"].type.subtype, j.data.types.Float)
+        and schema.props["llist3"].type.default == [1, 2, 3]
+    )
+    del schema.props["llist3"].type
     assert schema.props["llist3"].__dict__ == {
-        "type": "LF",
         "defaultvalue": "[1,2,3]",
         "index": False,
         "index_key": False,
@@ -62,8 +77,9 @@ def test_schema():
         "name": "llist3",
     }
     assert "nr" in schema.props
+    assert isinstance(schema.props["nr"].type, j.data.types.Integer) and schema.props["nr"].type.default == 4
+    del schema.props["nr"].type
     assert schema.props["nr"].__dict__ == {
-        "type": "i",
         "defaultvalue": "4",
         "index": True,
         "index_key": False,
@@ -73,8 +89,13 @@ def test_schema():
         "name": "nr",
     }
     assert "date_start" in schema.props
+
+    assert (
+        isinstance(schema.props["date_start"].type, j.data.types.Integer)
+        and schema.props["date_start"].type.default == 0
+    )
+    del schema.props["date_start"].type
     assert schema.props["date_start"].__dict__ == {
-        "type": "D",
         "defaultvalue": "0",
         "index": False,
         "index_key": False,
@@ -84,8 +105,12 @@ def test_schema():
         "name": "date_start",
     }
     assert "description" in schema.props
+    assert (
+        isinstance(schema.props["description"].type, j.data.types.String)
+        and schema.props["description"].type.default == ""
+    )
+    del schema.props["description"].type
     assert schema.props["description"].__dict__ == {
-        "type": "str",
         "defaultvalue": "",
         "index": True,
         "index_key": False,
@@ -95,8 +120,13 @@ def test_schema():
         "name": "description",
     }
     assert "description2" in schema.props
+    assert "description" in schema.props
+    assert (
+        isinstance(schema.props["description2"].type, j.data.types.String)
+        and schema.props["description2"].type.default == ""
+    )
+    del schema.props["description2"].type
     assert schema.props["description2"].__dict__ == {
-        "type": "S",
         "defaultvalue": "",
         "index": False,
         "index_key": True,
@@ -106,8 +136,13 @@ def test_schema():
         "name": "description2",
     }
     assert "llist4" in schema.props
+    assert (
+        isinstance(schema.props["llist4"].type, j.data.types.List)
+        and isinstance(schema.props["llist4"].type.subtype, j.data.types.Integer)
+        and schema.props["llist4"].type.default == [1, 2, 3]
+    )
+    del schema.props["llist4"].type
     assert schema.props["llist4"].__dict__ == {
-        "type": "L",
         "defaultvalue": "[1,2,3]",
         "index": False,
         "index_key": False,
@@ -117,8 +152,13 @@ def test_schema():
         "name": "llist4",
     }
     assert "llist5" in schema.props
+    assert (
+        isinstance(schema.props["llist5"].type, j.data.types.List)
+        and isinstance(schema.props["llist5"].type.subtype, j.data.types.Integer)
+        and schema.props["llist5"].type.default == [1, 2, 3]
+    )
+    del schema.props["llist5"].type
     assert schema.props["llist5"].__dict__ == {
-        "type": "LI",
         "defaultvalue": "[1,2,3]",
         "index": False,
         "index_key": False,
@@ -128,9 +168,14 @@ def test_schema():
         "name": "llist5",
     }
     assert "llist6" in schema.props
+    assert (
+        isinstance(schema.props["llist6"].type, j.data.types.List)
+        and isinstance(schema.props["llist6"].type.subtype, j.data.types.Integer)
+        and schema.props["llist6"].type.default == [1, 2, 3]
+    )
+    del schema.props["llist6"].type
     assert schema.props["llist6"].__dict__ == {
-        "type": "LI",
-        "defaultvalue": "1,2,3",
+        "defaultvalue": "[1,2,3]",
         "index": False,
         "index_key": False,
         "index_text": False,
@@ -139,8 +184,9 @@ def test_schema():
         "name": "llist6",
     }
     assert "U" in schema.props
+    assert isinstance(schema.props["U"].type, j.data.types.Float) and schema.props["U"].type.default == 0.0
+    del schema.props["U"].type
     assert schema.props["U"].__dict__ == {
-        "type": "float",
         "defaultvalue": "0.0",
         "index": False,
         "index_key": False,
@@ -150,8 +196,11 @@ def test_schema():
         "name": "U",
     }
     assert "nrdefault" in schema.props
+    assert (
+        isinstance(schema.props["nrdefault"].type, j.data.types.Integer) and schema.props["nrdefault"].type.default == 0
+    )
+    del schema.props["nrdefault"].type
     assert schema.props["nrdefault"].__dict__ == {
-        "type": "i",
         "defaultvalue": "0",
         "index": False,
         "index_key": False,
@@ -161,8 +210,12 @@ def test_schema():
         "name": "nrdefault",
     }
     assert "nrdefault2" in schema.props
+    assert (
+        isinstance(schema.props["nrdefault2"].type, j.data.types.Integer)
+        and schema.props["nrdefault2"].type.default == 0
+    )
+    del schema.props["nrdefault2"].type
     assert schema.props["nrdefault2"].__dict__ == {
-        "type": "I",
         "defaultvalue": "",
         "index": False,
         "index_key": False,
@@ -172,8 +225,12 @@ def test_schema():
         "name": "nrdefault2",
     }
     assert "nrdefault3" in schema.props
+    assert (
+        isinstance(schema.props["nrdefault3"].type, j.data.types.Integer)
+        and schema.props["nrdefault3"].type.default == 0
+    )
+    del schema.props["nrdefault3"].type
     assert schema.props["nrdefault3"].__dict__ == {
-        "type": "I",
         "defaultvalue": "0",
         "index": False,
         "index_key": False,
@@ -183,8 +240,11 @@ def test_schema():
         "name": "nrdefault3",
     }
     assert "obj" in schema.props
+    assert (
+        isinstance(schema.props["obj"].type, j.data.types.JSObject) and schema.props["obj"].type.default == "7mada.test"
+    )
+    del schema.props["obj"].type
     assert schema.props["obj"].__dict__ == {
-        "type": "O",
         "defaultvalue": "7mada.test",
         "index": False,
         "index_key": False,
