1886a36f8340b4e73b1945e4da6493cfbcc18765
Omar Elsheekh <omarashraf0109@gmail.com>
2019-08-27 15:33:00 +0200

    update token

--- tests/clients/github/test_githubclient.py
+++ tests/clients/github/test_githubclient.py
@@ -4,7 +4,7 @@
 # username: fakeForTest
 # name: Codescalers Test
 # organization: fakeForTest2
-# access token: 620d714c58c9dd5b07106e2117fa4de6e5fde315
+# access token: 80b2dbe2655694af337b1ada7ae83eb7d340a4d4
 
 from jumpscale.god import j
 import pytest
@@ -19,7 +19,7 @@ def test_github_client():
     # test with accesstoken
     j.clients.github.delete('omar')
     j.clients.github.new("omar")
-    j.clients.github.omar.accesstoken = "620d714c58c9dd5b07106e2117fa4de6e5fde315"
+    j.clients.github.omar.accesstoken = "80b2dbe2655694af337b1ada7ae83eb7d340a4d4"
     assert j.clients.github.omar.get_userdata()["name"] == "Codescalers Test"
     j.clients.github.omar.create_repo("hi")
     assert "hi" in j.clients.github.omar.get_repos()
