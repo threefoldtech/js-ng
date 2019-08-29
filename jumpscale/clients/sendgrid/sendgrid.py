83595f1ae8a0fd85cadcea9285b6c75a603eeaf2
Omar Elsheekh <omarashraf0109@gmail.com>
2019-08-29 11:27:35 +0200

    fix send email with attachment

--- jumpscale/clients/sendgrid/sendgrid.py
+++ jumpscale/clients/sendgrid/sendgrid.py
@@ -1,50 +1,52 @@
-from jumpscale.clients.base import Client
-from jumpscale.core.base import fields
 import base64
 import os
+import io
 import sendgrid
-from sendgrid.helpers.mail import  Mail, Attachment 
+from sendgrid.helpers.mail import Mail, Attachment
+from python_http_client.exceptions import HTTPError
+from jumpscale.god import j
+from jumpscale.clients.base import Client
+from jumpscale.core.base import fields
 
 
 class SendGridClient(Client):
+    apikey = fields.String()
+
     def __init__(self):
         super().__init__()
-        self.apikey = fields.String()
 
     def build_attachment(self, filepath, typ="application/pdf"):
         """
         Returns a valid sendgrid attachment from typical attachment object.
         """
-        with open(filepath, encoding="utf-8") as fp:
-            data = fp.read()
-        attachment=Attachment()
-        attachment.set_content(base64.b64encode(data))
-        attachment.set_type(typ)
-        attachment.set_filename(os.path.basename(filepath))
-        attachment.set_disposition("attachment")
+        data = io.BytesIO()
+        with open(filepath, "rb") as f:
+            while True:
+                d = f.read(2 ** 20)
+                if not d:
+                    break
+                data.write(d)
+        data.seek(0)
+        attachment = sendgrid.Attachment()
+        # attachment.set_content(base64.b64encode(data))
+        attachment.file_content = j.data.serializers.base64.encode(data.read()).decode()
+        attachment.file_type = typ
+        attachment.file_name = os.path.basename(filepath)
+        attachment.disposition = "attachment"
         return attachment
 
-        
-
-
-    def send(self, sender, subject, html_content='<strong>Email</strong>', recipients=[], attachments=[]):
-        recipients=list(set(recipients))
-        mail=Mail(
-            from_email=sender,
-            to_emails=recipients,
-            subject=subject,
-            html_content=html_content)
+    def send(self, sender, subject, html_content="<strong>Email</strong>", recipients=[], attachments=[]):
+        recipients = list(set(recipients))
+        mail = Mail(from_email=sender, to_emails=recipients, subject=subject, html_content=html_content)
         for at in attachments:
             mail.add_attachment(at)
         try:
-            sg=sendgrid.SendGridAPIClient(self.apikey)
-            #response=sg.send(mail)
+            sg = sendgrid.SendGridAPIClient(self.apikey)
+            # response=sg.send(mail)
             response = sg.client.mail.send.post(request_body=mail.get())
             print(response.status_code)
             print(response.body)
             print(response.headers)
-        except Exception as e:
-            print(str(e))
-
-
+        except HTTPError as e:
+            print(e.to_dict)
 
