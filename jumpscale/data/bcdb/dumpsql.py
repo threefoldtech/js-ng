4b539daba0b83246d40b712f4963821fc9dec504
xmonader <xmonader@gmail.com>
2019-09-03 15:52:41 +0200

    regenerate docs

--- jumpscale/data/bcdb/dumpsql.py
+++ jumpscale/data/bcdb/dumpsql.py
@@ -1,9 +1,14 @@
-import sqlite3
-from pprint import pprint
+def main():
+    import sqlite3
+    from pprint import pprint
 
-conn = sqlite3.connect("test_index.db")
-c = conn.cursor()
-c.execute("SELECT * FROM employee_name")
-pprint(c.fetchall())
-c.execute("SELECT * FROM employee_salary")
-pprint(c.fetchall())
+    conn = sqlite3.connect("test_index.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM employee_name")
+    pprint(c.fetchall())
+    c.execute("SELECT * FROM employee_salary")
+    pprint(c.fetchall())
+
+
+if __name__ == "__main__":
+    main()
--- jumpscale/data/bcdb/flush.py
+++ jumpscale/data/bcdb/flush.py
@@ -1,16 +1,21 @@
-import os
-from jumpscale.god import j
+def main():
+    import os
+    from jumpscale.god import j
 
-with open("test_index.db", "w"):
-    pass
+    with open("test_index.db", "w"):
+        pass
 
-print(os.system("redis-cli --scan  --pattern 'test.indexer.quote.id://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.quote://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.quote.las*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.indexer.employee.id://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.employee://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.employee.las*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.indexer.db.id://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.db://*' | xargs redis-cli del"))
-print(os.system("redis-cli --scan  --pattern 'test.db.las*' | xargs redis-cli del"))
-j.clients.sonic.get('test').flush_collection('test')
+    print(os.system("redis-cli --scan  --pattern 'test.indexer.quote.id://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.quote://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.quote.las*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.indexer.employee.id://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.employee://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.employee.las*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.indexer.db.id://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.db://*' | xargs redis-cli del"))
+    print(os.system("redis-cli --scan  --pattern 'test.db.las*' | xargs redis-cli del"))
+    j.clients.sonic.get("test").flush_collection("test")
+
+
+if __name__ == "__main__":
+    main()
