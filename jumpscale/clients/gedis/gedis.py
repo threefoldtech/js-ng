950139606ab7a893eb37a98218acbff13fed00ca
xmonader <xmonader@gmail.com>
2019-09-04 19:59:38 +0200

    Add doc strings for gedis

--- jumpscale/clients/gedis/gedis.py
+++ jumpscale/clients/gedis/gedis.py
@@ -3,22 +3,23 @@ from jumpscale.core.base import fields
 from jumpscale.god import j
 from functools import partial
 import json
+from typing import List
 
 """
-JS-NG> gedis = j.clients.gedis.get("local")                                                                                   
-JS-NG> gedis.list_actors()                                                                                                    
+JS-NG> gedis = j.clients.gedis.get("local")
+JS-NG> gedis.list_actors()
 ['system']
 
-JS-NG> gedis.register_actor("greeter", "/home/ahmed/wspace/js-next/js-ng/jumpscale/servers/gedis/example_greeter.py")         
+JS-NG> gedis.register_actor("greeter", "/home/ahmed/wspace/js-next/js-ng/jumpscale/servers/gedis/example_greeter.py")
 1
 
-JS-NG> gedis.list_actors()                                                                                                    
+JS-NG> gedis.list_actors()
 ['system', 'greeter']
 
-JS-NG>  
+JS-NG>
 
 
-JS-NG> gedis.ppdoc("greeter")                                                                                                 
+JS-NG> gedis.ppdoc("greeter")
 {
   "add2": {
     "args": [
@@ -45,37 +46,57 @@ JS-NG> gedis.ppdoc("greeter")
     "doc": "\n        \n        "
   }
 }
-JS-NG> gedis.execute("greeter", "hi")                                                                                               
+JS-NG> gedis.execute("greeter", "hi")
 b'hello world'
 
-JS-NG> gedis.execute("greeter", "ping")                                                                                             
+JS-NG> gedis.execute("greeter", "ping")
 b'pong no?'
 
-JS-NG> gedis.execute("greeter", "add2", "first", "second")                                                                          
+JS-NG> gedis.execute("greeter", "add2", "first", "second")
 b'firstsecond'
 
 
-JS-NG> gedis = j.clients.gedis.get("local")                                                                
-JS-NG> gedis.actors.greeter.hi()                                                                           
+JS-NG> gedis = j.clients.gedis.get("local")
+JS-NG> gedis.actors.greeter.hi()
 b'hello world'
 
-JS-NG> gedis.actors.greeter.add2("a", "b")                                                                 
+JS-NG> gedis.actors.greeter.add2("a", "b")
 b'ab'
-JS-NG>                                                                                                     
+JS-NG>
 """
 
 
 class ActorProxy:
     def __init__(self, actor_name, actor_info, gedis_client):
-        ## {method: 'method_name', args: [], 'doc':...}
+        """ActorProxy to remote actor on the server side
+
+        Arguments:
+            actor_name {str} -- [description]
+            actor_info {dict} -- actor information dict e.g { method_name: { args: [], 'doc':...} }
+            gedis_client {GedisClient} -- gedis client reference
+        """
         self.actor_name = actor_name
         self.actor_info = actor_info
         self._gedis_client = gedis_client
 
     def __dir__(self):
+        """Delegate the available functions on the ActorProxy to `actor_info` keys
+
+        Returns:
+            list -- methods available on the ActorProxy
+        """
         return list(self.actor_info.keys())
 
     def __getattr__(self, attr):
+        """Return a function representing the remote function on the actual actor
+
+        Arguments:
+            attr {str} -- method name
+
+        Returns:
+            function -- function waiting on the arguments
+        """
+
         def mkfun(actor_name, fn_name, *args):
             return self._gedis_client.execute(self.actor_name, fn_name, *args)
 
@@ -85,6 +106,11 @@ class ActorProxy:
 
 class ActorsCollection:
     def __init__(self, gedis_client):
+        """ActorsCollection to allow using the actors like `gedis.actors.ACTORNAME.ACTORMETHOD(*ACTOR_METHOD_ARGS)
+
+        Arguments:
+            gedis_client {GedisClient} -- gedis client
+        """
         self._gedis_client = gedis_client
         self._actors = {}
 
@@ -97,6 +123,14 @@ class ActorsCollection:
         return self.actors_names
 
     def _load_actor(self, actor_name):
+        """Load actor: creating ActorProxy for remote actor `actor_name` and store it in the collection.
+
+        Arguments:
+            actor_name {str} -- remote actor name
+
+        Returns:
+            ActorProxy -- ActorProxy that can call the remote actor.
+        """
         actor_info = json.loads(self._gedis_client.execute(actor_name, "info"))
         self._actors[actor_name] = ActorProxy(actor_name, actor_info, self._gedis_client)
         return self._actors[actor_name]
@@ -132,19 +166,49 @@ class GedisClient(Client):
         self._redisclient.save()
         return self._redisclient
 
-    def register_actor(self, actor_name, actor_path):
+    def register_actor(self, actor_name: str, actor_path: str):
+        """Register actor on the server side (gedis server)
+
+        Arguments:
+            actor_name {str} -- actor name to be used in the system
+            actor_path {str} -- actor path on the remote gedis server
+
+        """
         return self.execute("system", "register_actor", actor_name, actor_path)
 
-    def execute(self, actor_name, *args):
-        return self._redisclient.execute_command(actor_name, *args)
+    def execute(self, actor_name: str, actor_method: str, *args):
+        """Execute
 
-    def doc(self, actor_name):
+        Arguments:
+            actor_name {str} -- actor name
+            actor_name {str} -- actor method to execute
+            *args      {List[object]}  -- *args of parameters
+
+        """
+        return self._redisclient.execute_command(actor_name, actor_method, *args)
+
+    def doc(self, actor_name: str):
+        """Gets the documentation of actor `actor_name`
+
+        Arguments:
+            actor_name {str} -- actor to retrieve its documentation
+
+        """
         return json.loads(self.execute(actor_name, "info"))
 
     def ppdoc(self, actor_name):
+        """Pretty print documentation of actor
+
+        Arguments:
+            actor_name {str} -- actor to print its documentation.
+        """
         res = self.doc(actor_name)
         print(json.dumps(res, indent=2, sort_keys=True))
 
-    def list_actors(self):
-        return json.loads(self.execute("system", "list_actors"))
+    def list_actors(self) -> List[str]:
+        """List actors
 
+        Returns:
+            List[str] -- list of actors available on gedis server.
+        """
+        return json.loads(self.execute("system", "list_actors"))
