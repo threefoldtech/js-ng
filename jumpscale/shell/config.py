cc34a698b35c1d7827cc85db870d49cbcb60bf84
xmonader <xmonader@gmail.com>
2019-09-11 21:30:48 +0200

    add get_app missing import

--- jumpscale/shell/config.py
+++ jumpscale/shell/config.py
@@ -3,7 +3,7 @@ import traceback
 import better_exceptions
 
 from functools import partial
-
+from prompt_toolkit.application import get_app
 from prompt_toolkit.keys import Keys
 from prompt_toolkit.formatted_text import ANSI
 from prompt_toolkit.shortcuts import print_formatted_text
