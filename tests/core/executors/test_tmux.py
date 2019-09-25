a5570ab8fddb7431d7dac7d0fa9e30fb09814176
0xIslamTaha <islamtaha2012@gmail.com>
2019-09-25 16:41:29 +0200

    tests

--- tests/core/executors/test_tmux.py
+++ tests/core/executors/test_tmux.py
@@ -7,22 +7,24 @@ class TestTmux(BaseTests):
     def test001_create_session(self):
         session_name = self.generate_random_text()
         self.info("Create session with name : {}".format(session_name))
-        j.core.executors.tmux.create_session(session_name)
+        session = j.core.executors.tmux.create_session(session_name)
         self.assertIn(session_name, str(j.core.executors.tmux.server.sessions[0]))
+        self.info('Kill session')
+        session.kill_session()
 
-    #@skip('https://github.com/js-next/js-ng/issues/111')
     def test002_create_existing_session(self):
         session_name = self.generate_random_text()
         self.info("Create session with name : {}".format(session_name))
-        j.core.executors.tmux.create_session(session_name)
-        j.core.executors.tmux.create_session(session_name)
+        session = j.core.executors.tmux.create_session(session_name)
+        with self.assertRaises(Exception):
+            j.core.executors.tmux.create_session(session_name)
         self.assertIn(session_name, str(j.core.executors.tmux.server.sessions[0]))
+        self.info('Kill session')
+        session.kill_session()
 
-    #@skip("https://github.com/js-next/js-ng/issues/108")
     def test003_get_js_session(self):
         self.info("Get js session, should pass")
         self.assertIn(j.core.executors.tmux.JS_SESSION_NAME, str(j.core.executors.tmux.get_js_session()))
 
-    #@skip('https://github.com/js-next/js-ng/issues/109')
     def test004_get_js_window(self):
         self.assertIn(j.core.executors.tmux.JS_WINDOW_NAME, str(j.core.executors.tmux.get_js_window()))
