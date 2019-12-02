79f436b782bbf802bb4898bac2c7168f88635af2
Abdelrahman Hussein <ahus@hellofresh.com>
2019-12-02 18:22:16 +0100

    use benchmarkit

--- tests/data/bcdb/load_tests/benchmark_bcdb.py
+++ tests/data/bcdb/load_tests/benchmark_bcdb.py
@@ -1,6 +1,10 @@
 from jumpscale.god import j
 import string
 import random
+from benchmarkit import benchmark, benchmark_run
+import pprint
+
+SAVE_PATH = './bcdb_benchmark_time.jsonl'
 
 
 def generate_string(length):
@@ -9,19 +13,28 @@ def generate_string(length):
 
 
 db = j.data.bcdb.BCDB("load_test")
-quotes_model = self.db.get_model_by_name("quote")
+quotes_model = db.get_model_by_name("quote")
 
-
+@benchmark(num_iters=1000, save_params=True, save_output=False)
 def create_obj():
     obj = quotes_model.create_obj({"author": generate_string(random.randint(1, 20)), "quote": generate_string(random.randint(1, 150))})
     obj.save()
 
+
+@benchmark(num_iters=10000, save_params=True, save_output=False)
 def find_obj():
     quotes_model.get_pattern("quote", generate_string(random.randint(1, 50)))
 
 
 
+def run():
+    benchmark_results = benchmark_run(
+        [create_obj, find_obj],
+        SAVE_PATH,
+        comment="BCDB benchmark test",
+        metric="mean_time",
+    )
+    return benchmark_results
+
 if __name__ == "__main__":
-    for i in rane(10000):
-        create_obj()
-        find_obj()    
+    pprint.pprint(run())
