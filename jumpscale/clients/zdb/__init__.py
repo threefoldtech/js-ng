c231768aa7814a40bc4be06f4284328c6a4f84d4
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-04-09 13:24:06 +0200

    importing zdb clients including sequential and user modes

--- jumpscale/clients/zdb/__init__.py
+++ jumpscale/clients/zdb/__init__.py
@@ -1,5 +1,11 @@
 from jumpscale.core.base import StoredFactory
-from .client import ZDBClient, ZDBAdminClient
+from .client import ZDBClientSeqMode
 
-regular = StoredFactory(ZDBClient)
-admin = StoredFactory(ZDBAdminClient)
+# userclient = StoredFactory(ZDBClient)
+# adminclient = StoredFactory(ZDBAdminClient)
+
+
+def get(name="test_instance", addr="localhost", port=9900, secret_="12345566", nsname="test", admin=False, mode="seq"):
+    if admin == False and mode == "seq":
+        return StoredFactory(ZDBClientSeqMode).get(name, addr, port, secret_, nsname, admin, mode)
+
