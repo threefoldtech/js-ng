43a211891fddeb2ff6e952666c078d59e28eecb6
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-04-09 23:25:13 +0200

    use events system with redis to get a new client on property change

--- jumpscale/clients/redis/redis.py
+++ jumpscale/clients/redis/redis.py
@@ -1,15 +1,35 @@
-from jumpscale.clients.base import Client
-from jumpscale.core.base import fields
 from redis import Redis
 
+from jumpscale.core import events
 
-class RedisClient(Client):
+from jumpscale.clients.base import Client
+from jumpscale.core.base import fields
+from jumpscale.core.base.events import AttributeUpdateEvent
+
+
+class ClientAttributeUpdated(AttributeUpdateEvent):
+    pass
+
+
+class RedisClient(events.Handler, Client):
     hostname = fields.String(default="localhost")
     port = fields.Integer(default=6379)
 
     def __init__(self):
         self.__client = None
         super().__init__()
+
+        # listen to this event
+        events.add_listenter(self, ClientAttributeUpdated)
+
+    def _attr_updated(self, name, value):
+        # this will allow other people to listen to this event too
+        event = ClientAttributeUpdated(self, name, value)
+        events.notify(event)
+
+    def handle(self, ev):
+        if ev.instance == self:
+            self.__client = None
 
     def __dir__(self):
         return list(self.__dict__.keys()) + dir(self.redis_client)
