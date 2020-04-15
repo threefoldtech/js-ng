295e7fc5564428180669b418fc53ddb076f06192
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-04-15 18:39:04 +0200

    refactored droplets in digital ocean client

--- jumpscale/clients/digitalocean/digitalocean.py
+++ jumpscale/clients/digitalocean/digitalocean.py
@@ -16,15 +16,6 @@ import digitalocean
 from jumpscale.core.base import StoredFactory
 
 
-class VM:
-    name = fields.String()
-    do_id = fields.String()
-
-
-def convert_to_valid_identifier(name):
-    return "_" + name.replace(" ", "_")
-
-
 class ProjectFactory(StoredFactory):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -53,9 +44,6 @@ class Project(Client):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def create_project(self):
-        print("project created")
 
     def set_digital_ocean_name(self, name):
         self.do_name = name
@@ -93,8 +81,6 @@ class Project(Client):
         if is_default:
             project.update(is_default=True)
 
-        # self._projects.append(project) this for cashing
-
         return project
 
     def delete_remote(self):
@@ -108,30 +94,152 @@ class Project(Client):
         project = self.parent.projects.get_project_exist_remote(self.do_name)
         project.delete()
 
-        # self._projects.remove(project)
+
+class DropletFactory(StoredFactory):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def list_remote(self, project_name=None):
+        # List remote dorplet for a project if it is specified
+        if project_name:
+            project = self.parent_instance.projects.get_project_exist_remote(project_name)
+            return project.list_droplets()
+
+        return self.parent_instance.client.get_all_droplets()
+
+    def check_droplet_exist_remote(self, name):
+        for droplet in self.list_remote():
+            if droplet.name.lower() == name.lower():
+                return True
+        return False
+
+    def get_droplet_exist_remote(self, name):
+        for droplet in self.list_remote():
+            if droplet.name.lower() == name.lower():
+                return droplet
+        raise j.exceptions.Input("could not find project with name:%s on you Digital Ocean account" % name)
+
+    def shutdown_all(self, project_name=None):
+        for droplet in self.list_remote(project_name):
+            droplet.shutdown()
+
+    def delete_all(self, ignore=None, interactive=True, project_name=None):
+
+        if not ignore:
+            ignore = []
+
+        def test(ignore, name):
+            if name.startswith("TF-"):
+                return False
+            for item in ignore:
+                if name.lower().find(item.lower()) != -1:
+                    return False
+            return True
+
+        todo = []
+        for droplet in self.list_remote(project_name):
+            if test(ignore, droplet.name):
+                todo.append(droplet)
+        if todo != []:
+            todotxt = ",".join([i.name for i in todo])
+            if not interactive or j.tools.console.ask_yes_no("ok to delete:%s" % todotxt):
+                for droplet in todo:
+                    droplet.destroy()
 
 
-# class Droplet
+class Droplet(Client):
+    do_name = fields.String()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def set_digital_ocean_name(self, name):
+        self.do_name = name
+
+    def deploy(
+        self,
+        sshkey=None,
+        region="Amsterdam 3",
+        image="ubuntu 18.04",
+        size_slug="s-1vcpu-2gb",
+        delete=True,
+        project_name=None,
+    ):
+        """
+        :param name:
+        :param sshkey:
+        :param region:
+        :param image:
+        :param size_slug: s-1vcpu-2gb,s-6vcpu-16gb,gd-8vcpu-32gb
+        :param delete:
+        :param mosh: when mosh will be used to improve ssh experience
+        :param project_name: project to add this droplet it. If not specified the default project will be used.
+        :return: droplet,sshclient
+        """
+        project = None
+        if project_name:
+            project = self.parent.projects.get_project_exist_remote(self.do_name)
+            if not project:
+                raise j.exceptions.Input("could not find project with name:%s" % project_name)
+
+        # Get ssh
+        if not sshkey:
+            sshkey_do = self.parent._sshkey_get_default()
+
+            if not sshkey_do:
+                # means we did not find the sshkey on digital ocean yet, need to create
+                # TODO need to make sure that that sshkey is loaded and valid
+                sshkey = self.parent.sshkey
+                key = digitalocean.SSHKey(
+                    token=self.parent.projects.parent_instance.token_, name=sshkey.name, public_key=sshkey.public_key
+                )
+                key.create()
+            sshkey_do = self.parent._sshkey_get_default()
+            assert sshkey_do
+            sshkey = sshkey_do.name
+
+        if self.parent.droplets.check_droplet_exist_remote(self.do_name):
+            dr0 = self.parent.droplets.get_droplet_exist_remote(self.do_name)
+            if delete:
+                dr0.destroy()
+            else:
+                sshcl = j.clients.sshclient.get(name="do_%s" % self.do_name, host=dr0.ip_address, sshkey=sshkey)
+                return dr0, sshcl
+
+        sshkey = self.parent.droplets.parent_instance.sshkey_get(sshkey)
+        region = self.parent.droplets.parent_instance.region_get(region)
+        imagedo = self.parent.droplets.parent_instance.image_get(image)
+
+        img_slug_or_id = imagedo.slug if imagedo.slug else imagedo.id
+
+        droplet = digitalocean.Droplet(
+            token=self.parent.droplets.parent_instance.token_,
+            name=self.do_name,
+            region=region.slug,
+            image=img_slug_or_id,
+            size_slug=size_slug,
+            ssh_keys=[sshkey],
+            backups=False,
+        )
+        droplet.create()
+
+        if project:
+            project.assign_resources(["do:droplet:%s" % droplet.id])
+
+    def delete_remote(self):
+        droplet = self.parent.droplets.get_droplet_exist_remote(self.do_name)
+        droplet.destroy()
 
 
 class DigitalOcean(Client):
     name = fields.String()
     token_ = fields.String()
-    vms = fields.List(fields.Object(VM))
     projects = fields.Factory(Project, factory_type=ProjectFactory)
+    droplets = fields.Factory(Droplet, factory_type=DropletFactory)
 
     def __init__(self):
         super().__init__()
         self._client = None
-        self.reset()
-
-    def reset(self):
-        self._droplets = []
-        self._projects = []  # list_all
-        self._digitalocean_images = None
-        self._digitalocean_sizes = None
-        self._digitalocean_regions = None
-        self._sshkeys = None
 
     @property
     def client(self):
@@ -146,77 +254,22 @@ class DigitalOcean(Client):
             self._client = digitalocean.Manager(token=self.token_)
         return self._client
 
+    # Images
     @property
     def digitalocean_images(self):
-        if not self._digitalocean_images:
-            self._digitalocean_images = self.client.get_distro_images()
-        return self._digitalocean_images
+        return self.client.get_distro_images()
 
     @property
     def digitalocean_myimages(self):
         return self.client.get_images(private=True)
 
     @property
-    def digitalocean_sizes(self):
-        if not self._digitalocean_sizes:
-            self._digitalocean_sizes = self.client.get_all_sizes()
-        return self._digitalocean_sizes
-
-    @property
-    def digitalocean_regions(self):
-        if not self._digitalocean_regions:
-            self._digitalocean_regions = self.client.get_all_regions()
-        return self._digitalocean_regions
-
-    @property
-    def digitalocean_region_names(self):
-        return [i.slug for i in self.digitalocean_regions]
-
-    @property
-    def sshkeys(self):
-        if not self._sshkeys:
-            self._sshkeys = self.client.get_all_sshkeys()
-        return self._sshkeys
-
-    def droplet_exists(self, name):
-        for droplet in self.droplets:
-            if droplet.name.lower() == name.lower():
-                return True
-        return False
-
-    def _droplet_get(self, name):
-        for droplet in self.droplets:
-            if droplet.name.lower() == name.lower():
-                return droplet
-        return False
-
-    def _sshkey_get_default(self):
-        pubkeyonly = self.sshkey.public_key
-        for item in self.sshkeys:
-            if item.public_key.find(pubkeyonly) != -1:
-                return item
-        return None
-
-    def sshkey_set_default(self, default_sshkey):
-        self.sshkey = default_sshkey
-
-    def sshkey_get(self, name):
-        for item in self.sshkeys:
-            if name == item.name:
-                return item
-        raise j.exceptions.Base("did not find key:%s" % name)
-
-    def region_get(self, name):
-        for item in self.digitalocean_regions:
-            if name == item.slug:
-                return item
-            if name == item.name:
-                return item
-        raise j.exceptions.Base("did not find region:%s" % name)
-
-    @property
     def digitalocean_account_images(self):
         return self.digitalocean_images + self.digitalocean_myimages
+
+    @property
+    def digitalocean_sizes(self):
+        return self.client.get_all_sizes()
 
     def image_get(self, name):
         for item in self.digitalocean_account_images:
@@ -242,154 +295,44 @@ class DigitalOcean(Client):
                 res.append(name_do)
         return res
 
-    def droplet_create(
-        self,
-        name="test",
-        sshkey=None,
-        region="Amsterdam 3",
-        image="ubuntu 18.04",
-        size_slug="s-1vcpu-2gb",
-        delete=True,
-        project_name=None,
-    ):
-        """
-
-        :param name:
-        :param sshkey:
-        :param region:
-        :param image:
-        :param size_slug: s-1vcpu-2gb,s-6vcpu-16gb,gd-8vcpu-32gb
-        :param delete:
-        :param mosh: when mosh will be used to improve ssh experience
-        :param project_name: project to add this droplet it. If not specified the default project will be used.
-        :return: droplet,sshclient
-        """
-        project = None
-        if project_name:
-            project = self._project_get(project_name)
-            if not project:
-                raise j.exceptions.Input("could not find project with name:%s" % project_name)
-
-        # Get ssh
-        if not sshkey:
-            sshkey_do = self._sshkey_get_default()
-
-            if not sshkey_do:
-                # means we did not find the sshkey on digital ocean yet, need to create
-                key = digitalocean.SSHKey(token=self.token_, name=self.sshkey.name, public_key=self.sshkey.public_key)
-                key.create()
-            sshkey_do = self._sshkey_get_default()
-            assert sshkey_do
-            sshkey = sshkey_do.name
-
-        if self.droplet_exists(name):
-            dr0 = self._droplet_get(name=name)
-            if delete:
-                dr0.destroy()
-            else:
-                sshcl = j.clients.sshclient.get(name="do_%s" % name, host=dr0.ip_address, sshkey=sshkey)
-                # sshcl.save()
-                return dr0, sshcl
-
-        sshkey = self.sshkey_get(sshkey)
-
-        region = self.region_get(region)
-
-        imagedo = self.image_get(image)
-
-        img_slug_or_id = imagedo.slug if imagedo.slug else imagedo.id
-        droplet = digitalocean.Droplet(
-            token=self.token_,
-            name=name,
-            region=region.slug,
-            image=img_slug_or_id,
-            size_slug=size_slug,
-            ssh_keys=[sshkey],
-            backups=False,
-        )
-        droplet.create()
-        self._droplets.append(droplet)
-        self.reset()
-
-        if project:
-            project.assign_resources(["do:droplet:%s" % droplet.id])
-
-        vm = self._vm_get(name)
-        vm.do_id = droplet.id
-
-    def _vm_get(self, name, new=True):
-        vm = None
-        for single_vm in self.vms:
-            if single_vm.name == name:
-                vm = single_vm
-                break
-        if new:
-            if not vm:
-                vm = VM()
-                vm.name = name
-                self.vms.append(vm)
-        return vm
-
-    def _vm_exists(self, name):
-        return self._vm_get(name, new=False) != None
-
-    def droplet_get(self, name):
-        if not self.droplet_exists(name):
-            raise j.exceptions.Input("could not find vm with name:%s" % name)
-        return self._droplet_get(name)
+    # Regions
 
     @property
-    def droplets(self):
-        if not self._droplets:
-            self._droplets = []
-            for d in self.client.get_all_droplets():
-                self._droplets.append(d)
-        return self._droplets
+    def digitalocean_regions(self):
+        return self.client.get_all_regions()
 
-    def droplets_all_delete(self, ignore=None, interactive=True, project=None):
+    @property
+    def digitalocean_region_names(self):
+        return [i.slug for i in self.digitalocean_regions]
 
-        if not ignore:
-            ignore = []
+    def region_get(self, name):
+        for item in self.digitalocean_regions:
+            if name == item.slug:
+                return item
+            if name == item.name:
+                return item
+        raise j.exceptions.Base("did not find region:%s" % name)
 
-        def test(ignore, name):
-            if name.startswith("TF-"):
-                return False
-            for item in ignore:
-                if name.lower().find(item.lower()) != -1:
-                    return False
-            return True
+    # SSHkeys
+    @property
+    def sshkeys(self):
+        return self.client.get_all_sshkeys()
 
-        todo = []
-        for droplet in self.droplets_list(project):
-            if test(ignore, droplet.name):
-                todo.append(droplet)
-        if todo != []:
-            todotxt = ",".join([i.name for i in todo])
-            if not interactive or j.tools.console.ask_yes_no("ok to delete:%s" % todotxt):
-                for droplet in todo:
-                    droplet.destroy()
+    def _sshkey_get_default(self):
+        pubkeyonly = self.sshkey.public_key
+        for item in self.sshkeys:
+            if item.public_key.find(pubkeyonly) != -1:
+                return item
+        return None
 
-    def droplets_all_shutdown(self, project=None):
-        for droplet in self.droplets_list(project):
-            droplet.shutdown()
+    def sshkey_set_default(self, default_sshkey):
+        self.sshkey = default_sshkey
 
-    def droplets_list(self, project=None):
-        """list droplets
-
-        :param project: name of the project to filter on, defaults to None
-        :type project: str, optional
-        :raises j.exceptions.Input: raise an error if project doesn't exist.
-        :return: list of droplets
-        :rtype: [Droplet]
-        """
-        if not project:
-            return self.droplets
-
-        project = self._project_get(project)
-        if not project:
-            raise j.exceptions.Input("could not find project with name:%s" % project)
-
-        return project.list_droplets()
+    def sshkey_get(self, name):
+        for item in self.sshkeys:
+            if name == item.name:
+                return item
+        raise j.exceptions.Base("did not find key:%s" % name)
 
     def __str__(self):
         return "digital ocean client:%s" % self.name
