e38c4e814608a10bbb823def2697c5fe1453f5eb
xmonader <xmonader@gmail.com>
2020-04-17 09:39:20 +0200

    improve jsync docs

--- jumpscale/entry_points/jsync.py
+++ jumpscale/entry_points/jsync.py
@@ -1,12 +1,12 @@
-"""syncing tool to sync over certain set of directories against remote machine.
-list available clients
+"""`jsync` is syncing tool to sync over certain set of directories against remote machine.
+## list available clients
 ```
 ~> poetry run jsync list-ssh-clients           
 ['xmonader']
-
 ```
 
-sync with a client certain set of paths
+## sync with a client certain set of paths
+```
 ~> poetry run jsync sync --clients "xmonader" --paths "~/wspace/tq,/tmp/proj:/tmp/proj2"
 ['xmonader'] {'~/wspace/tq': '~/wspace/tq', '/tmp/proj': '/tmp/proj2'}
 ```
