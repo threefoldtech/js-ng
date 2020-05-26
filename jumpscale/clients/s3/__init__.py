f9ef96845d8d4291ceba5fb3659798f3dfb1e3d1
xmonader <xmonader@gmail.com>
2020-05-26 09:47:26 +0200

    move github, s3, sendgrid, countries, dnstool, imagelib, profiler to sdk

--- jumpscale/clients/s3/__init__.py
+++
@@ -1,5 +0,0 @@
-def export_module_as():
-    from jumpscale.core.base import StoredFactory
-    from .s3 import S3Client
-
-    return StoredFactory(S3Client)
--- jumpscale/clients/sendgrid/__init__.py
+++
@@ -1,5 +0,0 @@
-def export_module_as():
-    from jumpscale.core.base import StoredFactory
-    from .sendgrid import SendGridClient
-
-    return StoredFactory(SendGridClient)
