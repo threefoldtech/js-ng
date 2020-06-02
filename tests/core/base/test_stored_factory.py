7060887590747509c243ba626334e232b7f9401e
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-06-02 18:28:25 +0200

    fields: add tests for on_update, compute and stored fields

--- tests/core/base/test_stored_factory.py
+++ tests/core/base/test_stored_factory.py
@@ -29,12 +29,41 @@ class UserType(Enum):
     ADMIN = "admin"
 
 
+class Greeter:
+    def __init__(self, name):
+        self.name = name
+
+    def say(self):
+        print("hello", self.name)
+
+
 class User(Base):
     emails = fields.List(fields.String())
     permissions = fields.List(fields.Object(Permission))
     custom_config = fields.Typed(dict)
     type = fields.Enum(UserType)
     password = fields.Secret()
+
+    first_name = fields.String(default="")
+    last_name = fields.String(default="")
+
+    def get_full_name(self):
+        name = self.first_name
+        if self.last_name:
+            name += " " + self.last_name
+        return name
+
+    def get_unique_name(self):
+        return self.full_name.replace(" ", "") + ".user"
+
+    full_name = fields.String(compute=get_full_name)
+    unique_name = fields.String(compute=get_unique_name)
+
+    def get_my_greeter(self):
+        return Greeter(self.full_name)
+
+    my_greeter = fields.Typed(Greeter, stored=False, compute=get_my_greeter)
+    ahmed_greeter = fields.Typed(Greeter, stored=False, default=Greeter("ahmed"))
 
 
 class Client(Base):
@@ -139,6 +168,68 @@ class TestStoredFactory(unittest.TestCase):
         self.assertNotEqual(cl, ret_cl)
         user = ret_cl.users.get("admin")
         self.assertEqual(user.type, UserType.ADMIN)
+
+    def test_create_with_non_stored_fields(self):
+        cl = self.factory.get("test_enum")
+
+        # test saving
+        user = cl.users.get("admin")
+        user.first_name = "abdo"
+        user.last_name = "tester"
+        user.my_greeter.say()
+        user.ahmed_greeter.say()
+
+        with self.assertRaises(fields.ValidationError):
+            user.ahmed_greeter = ""
+
+        data = user.to_dict()
+
+        self.assertFalse("my_greeter" in data)
+        self.assertFalse("ahmed_greeter" in data)
+
+        user.save()
+        cl.save()
+        # reset-factory for now, need to always get from store
+        self.factory = StoredFactory(Client)
+        ret_cl = self.factory.get("test_enum")
+
+        self.assertNotEqual(cl, ret_cl)
+        user = ret_cl.users.get("admin")
+
+        data = user.to_dict()
+
+        self.assertFalse("my_greeter" in data)
+        self.assertFalse("ahmed_greeter" in data)
+
+    def test_create_with_computed_fields(self):
+        cl = self.factory.get("test_enum")
+
+        # test saving
+        user = cl.users.get("admin")
+        # default value, still
+        self.assertEqual(user.type, UserType.USER)
+
+        # now set and test if it's saved
+        user.first_name = "ahmed"
+
+        self.assertEqual(user.full_name, user.first_name)
+        user.last_name = "mohamed"
+
+        full_name = f"{user.first_name} {user.last_name}"
+        self.assertEqual(user.full_name, full_name)
+
+        unique_name = f"{user.first_name}{user.last_name}.user"
+        self.assertEqual(user.unique_name, unique_name)
+        user.save()
+        cl.save()
+        # reset-factory for now, need to always get from store
+        self.factory = StoredFactory(Client)
+        ret_cl = self.factory.get("test_enum")
+
+        self.assertNotEqual(cl, ret_cl)
+        user = ret_cl.users.get("admin")
+        self.assertEqual(user.full_name, full_name)
+        self.assertEqual(user.unique_name, unique_name)
 
     def tearDown(self):
         for name in self.factory.store.list_all():
