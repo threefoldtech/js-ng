fb155b02c33d5a9864c8636348856ae33776e666
AhmedSa-mir <asamir97@hotmail.com>
2020-06-21 18:31:00 +0200

    Removed codeserver installation from image - Will be on codeserver package installation instead

--- jumpscale/install/codeserver-install.sh
+++
@@ -1,5 +0,0 @@
-cd /tmp/
-wget -c https://github.com/cdr/code-server/releases/download/2.1698/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
-tar -xvzf code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
-mkdir -p /root/sandbox/bin/
-cp code-server2.1698-vsc1.41.1-linux-x86_64/code-server /root/sandbox/bin/
