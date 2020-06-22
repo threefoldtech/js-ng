5ca25cda83f367a9e8b38abad67661b34edeee2b
alichaddad <chaddada@greenitglobe.com>
2020-06-22 14:57:53 +0200

    fix sshclient autocompletion and validation

--- jumpscale/clients/sshclient/sshclient.py
+++ jumpscale/clients/sshclient/sshclient.py
@@ -1,6 +1,6 @@
 """
 
-SSHClient modules helps connecting to a remote machine and 
+SSHClient modules helps connecting to a remote machine and
 
 
 ## Create SSH Key
@@ -34,11 +34,9 @@ from jumpscale.core.base import fields
 from jumpscale.god import j
 
 
-
 class SSHClient(Client):
     """
     SSHClient has the following properties:
-    name (str): the name to assign to the client 
     sshkey (str): sshkey to use within that client
     host (str): host ip
     user (str): user to connect as default: True
@@ -48,36 +46,40 @@ class SSHClient(Client):
 
     """
 
-    name = fields.String()
-    sshkey = fields.String()
+    sshkey = fields.String(required=True)
 
-    host = fields.String(default="127.0.0.1")
-    user = fields.String(default="root")
-    port = fields.Integer()
+    host = fields.String(default="127.0.0.1", required=True)
+    user = fields.String(default="root", required=True)
+    port = fields.Integer(required=True)
     forward_agent = fields.Boolean(default=True)
     connect_timeout = fields.Integer(default=10)
 
     # gateway = ?  FIXME: should help with proxyjumps. http://docs.fabfile.org/en/2.4/concepts/networking.html#ssh-gateways
     # connect_kwargs = ? FIXME: how to pass dict?
-    inline_ssh_env = fields.Boolean(default=True) # whether to send environment variables “inline” as prefixes in front of command strings (export VARNAME=value && mycommand here), instead of trying to submit them through the SSH protocol itself (which is the default behavior). This is necessary if the remote server has a restricted AcceptEnv setting (which is the common default).
+    inline_ssh_env = fields.Boolean(
+        default=True
+    )  # whether to send environment variables “inline” as prefixes in front of command strings (export VARNAME=value && mycommand here), instead of trying to submit them through the SSH protocol itself (which is the default behavior). This is necessary if the remote server has a restricted AcceptEnv setting (which is the common default).
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.__client = None
 
+    @property
     def _sshkey(self):
         return j.clients.sshkey.get(self.sshkey)
-    
+
     @property
     def sshclient(self):
+        self.validate()
         if not self.__client:
             connection_kwargs = dict(
-                                    host=self.host, user=self.user, port=self.port,
-                                    forward_agent=self.forward_agent,
-                                    connect_timeout=self.connect_timeout,
-                                    connect_kwargs={
-                                            "key_filename": self._sshkey().private_key_path,
-            })
+                host=self.host,
+                user=self.user,
+                port=self.port,
+                forward_agent=self.forward_agent,
+                connect_timeout=self.connect_timeout,
+                connect_kwargs={"key_filename": self._sshkey.private_key_path},
+            )
             # FIXME: coredump here.
             # if self._sshkey.passphrase:
             #     connection_kwargs["connect_kwargs"]["passphrase"] = self._sshkey().passphrase
@@ -88,9 +90,3 @@ class SSHClient(Client):
 
     def reset_connection(self):
         self.__client = None
-
-    def __getattr__(self, name):
-        return getattr(self.sshclient, name)
-
-    def __dir__(self):
-        return list(self.__dict__.keys()) + dir(self.sshclient)
