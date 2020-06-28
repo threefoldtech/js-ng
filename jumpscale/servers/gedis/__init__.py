0aacf35e41b668604f5aaed6a24ce81e46a8e9a2
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-06-28 16:29:55 +0200

    Update gedis server docs

--- jumpscale/servers/gedis/__init__.py
+++ jumpscale/servers/gedis/__init__.py
@@ -1,7 +1,23 @@
 """Gedis server package provides all code needed to have an RPC server using redis protocol for messaging
 
-# TODO: add code to show how to start gedis.
+### Create Gedis instance
 ```
+t = j.servers.gedis.get("test")
+```
+this will create Gedis instance with name test running on  default port 16000
+```
+t = j.servers.gedis.new("test",port=1500)
+```
+This will create Gedis instance with name test running on port 1500
+### Start Gedis server
+```
+t.start()
+```
+### Stop Gedis server
+```
+t.stop()
+```
+
 ~>  redis-cli -p 16000 greeter hi
 actor greeter isn't loaded
  ~>  redis-cli -p 16000 system register_actor greeter /home/ahmed/wspace/threefoldtech/js-ng/jumpscale/servers/gedis/example_greeter.py
@@ -31,4 +47,5 @@ from jumpscale.core.base import StoredFactory
 
 def export_module_as():
     from .server import GedisServer
+
     return StoredFactory(GedisServer)
