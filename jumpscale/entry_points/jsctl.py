62e1dc7e801efbf4f6941a6b0695444a6546d541
Omar Elawady <omarelawady1998@gmail.com>
2020-07-01 19:40:12 +0200

    Prettyprint get, handle keyerrors, type awareness

--- jumpscale/entry_points/jsctl.py
+++ jumpscale/entry_points/jsctl.py
@@ -1,10 +1,25 @@
 import click
 
 from jumpscale.core.config import get_default_config, update_config, get_config
+import toml
+
+
+def decode_toml_value(s):
+    try:
+        return toml.TomlDecoder().load_value(s)[0]
+    except ValueError:
+        return s
+
+
+def encode_toml_value(s):
+    return toml.TomlEncoder().dump_value(s)
 
 
 def format_config_parameter(name, value):
-    return "%s = %s" % (name, str(value))
+    if isinstance(value, dict):
+        print_dict(value, "")
+    else:
+        return "%s = %s" % (name, encode_toml_value(value))
 
 
 def print_dict(data, path):
@@ -12,7 +27,8 @@ def print_dict(data, path):
         if isinstance(value, dict):
             print_dict(value, f"{path}{name}.")
         else:
-            print(f"{path}{name} = {value}")
+            encoded = encode_toml_value(value)
+            print(f"{path}{name} = {encoded}")
 
 
 def format_config(config):
@@ -48,9 +64,14 @@ def list_all():
 @config.command()
 @click.argument("name")
 def get(name):
-    _, obj, prop = traverse_config(name)
-    value = obj[prop]
-    click.echo(format_config_parameter(name, value))
+    try:
+        _, obj, prop = traverse_config(name)
+        if not isinstance(obj, dict):
+            raise KeyError()
+        value = obj[prop]
+        click.echo(format_config_parameter(name, value))
+    except KeyError:
+        click.echo("Key doens't exist")
 
 
 @config.command()
@@ -59,14 +80,14 @@ def get(name):
 def update(name, value):
     try:
         config, obj, prop = traverse_config(name)
-        obj[prop] = value
+        if not isinstance(obj, dict) or prop not in obj:
+            raise KeyError()
+        obj[prop] = decode_toml_value(value)
         update_config(config)
     except KeyError:
-        config = get_config()
-        config[name] = value
-        update_config(config)
-
-    click.echo("Updated.")
+        click.echo("Key doen't exist")
+    else:
+        click.echo("Updated.")
 
 
 @click.group()
