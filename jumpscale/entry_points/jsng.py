d1f01cc21bea12e126a43c024b15aca35ddcd788
Jo De Boeck <deboeck.jo@gmail.com>
2020-07-01 16:24:02 +0200

    Allow configuring ipython as default shell
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- jumpscale/entry_points/jsng.py
+++ jumpscale/entry_points/jsng.py
@@ -1,15 +1,13 @@
 from gevent import monkey
 
-monkey.patch_all(subprocess=False)
+monkey.patch_all(subprocess=False)  # noqa: E402
 import click
 import os
 import pathlib
 import sys
 
-from jumpscale.loader import j
-from jumpscale.shell import ptconfig
-
-from ptpython.repl import embed
+from jumpscale.loader import j  # noqa:
+from jumpscale.core.config.config import get_config
 
 BASE_CONFIG_DIR = os.path.join(os.environ.get("HOME", "/root"), ".jsng")
 HISTORY_FILENAME = os.path.join(BASE_CONFIG_DIR, "history.txt")
@@ -22,6 +20,13 @@ def run(command):
     os.makedirs(BASE_CONFIG_DIR, exist_ok=True)
     pathlib.Path(HISTORY_FILENAME).touch()
     if command is None:
-        sys.exit(embed(globals(), locals(), configure=ptconfig, history_filename=HISTORY_FILENAME))
+        config = get_config()
+        if config["shell"] == "ipython":
+            from IPython import embed
+            sys.exit(embed(colors="neutral"))
+        else:
+            from jumpscale.shell import ptconfig
+            from ptpython.repl import embed
+            sys.exit(embed(globals(), locals(), configure=ptconfig, history_filename=HISTORY_FILENAME))
     else:
         sys.exit(print(eval(command)))
