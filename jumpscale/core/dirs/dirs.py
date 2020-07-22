aa8600dacbd62f1e43f90f0f0d9a871d3289d670
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2020-07-22 12:10:51 +0200

    Adds jscfgdir to dirs
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- jumpscale/core/dirs/dirs.py
+++ jumpscale/core/dirs/dirs.py
@@ -44,12 +44,13 @@ import os
 
 class Dirs:
 
-    HOMEDIR = os.path.expanduser("~")  # TODO: ؤاثؤن homedir defined in sal.fs
+    HOMEDIR = os.path.expanduser("~")  # TODO: check homedir defined in sal.fs
     BASEDIR = os.path.join(HOMEDIR, "sandbox")
     BINDIR = os.path.join(BASEDIR, "bin")
     CFGDIR = os.path.join(BASEDIR, "cfg")  # TODO: check conflict with core.config_root..
     CODEDIR = os.path.join(BASEDIR, "code")
     VARDIR = os.path.join(BASEDIR, "var")
     LOGDIR = os.path.join(VARDIR, "log")
+    JSCFGDIR = os.path.join(HOMEDIR, ".config/jumpscale")
     TEMPLATEDIR = os.path.join(VARDIR, "templates")
     TMPDIR = "/tmp/jumpscale"
