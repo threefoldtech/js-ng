56120f60046771d4aff252bd542e85fc992e996d
MElborolossy <mohammedelborolossy@gmail.com>
2020-09-30 13:27:13 +0200

    Update is_broken_link function

--- jumpscale/sals/fs/__init__.py
+++ jumpscale/sals/fs/__init__.py
@@ -400,9 +400,6 @@ def is_broken_link(path: str) -> bool:
                 False if path not found or symlink is not broken
     """
     return os.path.islink(path) and not exists(path)
-        return True
-    else:
-        return False
 
 
 def rm_broken_link(path: str) -> bool:
