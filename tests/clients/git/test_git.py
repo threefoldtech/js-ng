97826201906e658541174cb30c3ec565bf3fbdce
Mostafa Hassan <mhost39@gmail.com>
2020-10-14 11:48:06 +0200

    test

--- tests/clients/git/test_git.py
+++ tests/clients/git/test_git.py
@@ -8,7 +8,12 @@ class GitTests(BaseTests):
 
     def setUp(self):
         super().setUp()
-
+        self.instance_name = self.generate_random_text()
+        self.git = j.clients.git.new(self.instance_name)
+        #self.git.remote_url = 
 
     def tearDown(self):
+        pass
+
+    def test01_check_branch(self):
         pass
