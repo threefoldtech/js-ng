ecd550da0672ceaafc8a739a00c04640f0283189
Mostafa Hassan <mhost39@gmail.com>
2020-10-25 11:44:49 +0200

    add sshkey and sshclient test

--- jumpscale/clients/git/git.py
+++ jumpscale/clients/git/git.py
@@ -7,8 +7,8 @@ from jumpscale.loader import j
 class GitClient(Client):
     path = fields.String()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
         self.__repo = None
 
     @property
@@ -72,5 +72,3 @@ class GitClient(Client):
             self.repo.git.add("-A")
         if self.repo.index.diff("HEAD"):
             return self.repo.index.commit(message)
-
-
