81773e176cf676532dc50d81f337f036229bae6f
Abdelrahman Ghanem <abom.jdev@gmail.com>
2020-11-09 14:09:18 +0200

    add get_traceback helper method for error handler

--- jumpscale/tools/errorhandler/errorhandler.py
+++ jumpscale/tools/errorhandler/errorhandler.py
@@ -34,11 +34,49 @@ class ErrorHandler:
             frame = frame.tb_next
         return stacktrace
 
-    def _handle_exception(self, ttype, tvalue, tb, level=40, die=False, log=True, category="", data=None):
-        timestamp = j.data.time.now().timestamp
+    def get_traceback(self, exc_info=None):
+        """Get a trackback information as a dict, suitable to used with error/alert handlers
+
+        if `exe_info` is not passed, `sys.exc_info()` will be used instead.
+
+        Example traceback information:
+
+        ```
+        {
+            'process_id': 20840,
+            'raw': 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\nValueError: a is not valid',
+            'straceback': [
+                {
+                    'code': '',
+                    'context': '<module>',
+                    'filename': '<stdin>',
+                    'filepath': '<stdin>',
+                    'linenr': 1
+                }
+            ]
+        }
+        ```
+
+        Args:
+            exc_info (tuple, optional): exception information as a tuple (ttype, tvalue, tb). Defaults to None.
+
+        Returns:
+            dict: raw and stacktrace information as a dict, alongside process id
+        """
+        if exc_info:
+            ttype, tvalue, tb = exc_info
+        else:
+            ttype, tvalue, tb = sys.exc_info()
+
         stacktrace = self._construct_stacktrace(tb)
-        message = self._format_lines(traceback.format_exception_only(ttype, tvalue))
         traceback_text = self._format_lines(traceback.format_exception(ttype, tvalue, tb))
+
+        return {"raw": traceback_text, "straceback": stacktrace, "process_id": j.application.process_id}
+
+    def _handle_exception(self, ttype, tvalue, tb, level=40, die=False, log=True, category="", data=None):
+        exc_info = (ttype, tvalue, tb)
+        timestamp = j.data.time.now().timestamp
+        message = self._format_lines(traceback.format_exception_only(ttype, tvalue))
 
         err_dict = {
             "app_name": j.logger.default_app_name,
@@ -47,11 +85,11 @@ class ErrorHandler:
             "timestamp": timestamp,
             "category": category or "exception",
             "data": data,
-            "traceback": {"raw": traceback_text, "stacktrace": stacktrace, "process_id": j.application.process_id},
+            "traceback": self.get_traceback(exc_info),
         }
 
         if log:
-            exception = (ttype, tvalue, tb)
+            exception = exc_info
             j.logger.exception(message=message, category=category, data=data, level=level, exception=exception)
 
         for handler_func, handler_level in self.handlers:
