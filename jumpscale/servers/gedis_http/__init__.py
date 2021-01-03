7c69c80ab273475acb349f6bbad7e865950e8d36
ranatrk <rana.t.hassan@gmail.com>
2021-01-03 16:04:37 +0200

    Add cors header and options to gedis http server

--- jumpscale/servers/gedis_http/__init__.py
+++ jumpscale/servers/gedis_http/__init__.py
@@ -16,7 +16,7 @@ class GedisHTTPServer(Base):
         super().__init__(*args, **kwargs)
         self._app = Bottle()
         self._client = None
-        self._app.route("/<package>/<actor>/<method>", ["GET", "POST"], self.handler)
+        self._app.route("/<package>/<actor>/<method>", ["GET", "POST", "OPTIONS"], self.enable_cors(self.handler))
 
     @property
     def client(self):
@@ -29,6 +29,21 @@ class GedisHTTPServer(Base):
         response.status = code
         response.content_type = "application/json"
         return json.dumps(content)
+
+    def enable_cors(self, fn):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS, DELETE"
+            response.headers[
+                "Access-Control-Allow-Headers"
+            ] = "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+
+            if request.method != "OPTIONS":
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
 
     def handler(self, package, actor, method):
         actors = self.client.actors
