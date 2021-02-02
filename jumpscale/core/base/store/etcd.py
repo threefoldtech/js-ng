f5c9ba49196c509ba848978a4d55b12bf16cef63
ranatrk <rana.t.hassan@gmail.com>
2021-02-02 13:41:48 +0200

    Add basic functionalities for etcd configmanager storage

--- jumpscale/core/base/store/etcd.py
+++ jumpscale/core/base/store/etcd.py
@@ -1,7 +1,40 @@
 import etcd3
+import json
 
 from . import ConfigNotFound, EncryptedConfigStore
 from .serializers import JsonSerializer
 
+
 class EtcdStore(EncryptedConfigStore):
-    pass
+    def __init__(self, location):
+        super().__init__(location, JsonSerializer())
+        etcd_config = self.config_env.get_store_config("etcd")
+        hostname = etcd_config.get("hostname", "localhost")
+        port = etcd_config.get("port", 2379)
+        self.etcd_client = etcd3.client(host=hostname, port=port)
+
+    def get_key(self, instance_name):
+        return ".".join([self.location.name, instance_name])
+
+    def read(self, instance_name):
+        key = self.get_key(instance_name)
+        if not self.etcd_client.get(instance_name):
+            raise ConfigNotFound(f"cannot find config for {instance_name} at {key}")
+
+        return self.etcd_client.get(key)[0].decode()
+
+    def write(self, instance_name, data):
+        key = self.get_key(instance_name)
+        self.etcd_client.put(key, data)
+        "folder1.folder2.instance_name"
+
+    def list_all(self):
+        return [item[1].key.decode().split(".")[-1] for item in self.etcd_client.get_all()]
+
+    # def find(self, cursor_=None, limit_=None, **query):
+    #     pass
+
+    def delete(self, instance_name):
+        key = self.get_key(instance_name)
+        return self.etcd_client.delete(key)
+
