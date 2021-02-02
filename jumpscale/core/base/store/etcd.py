dab95e0c685acf294da62762e9862eac79ae2e09
MElborolossy <mohammedelborolossy@gmail.com>
2021-02-02 17:39:34 +0200

    Add docstring to etcd store

--- jumpscale/core/base/store/etcd.py
+++ jumpscale/core/base/store/etcd.py
@@ -7,7 +7,21 @@ from .serializers import JsonSerializer
 
 
 class EtcdStore(EncryptedConfigStore):
+    """
+    EtcdStore store is an EncryptedConfigStore
+
+    It saves the data in etcd and configuration for etcd comes from `config_env.get_store_config("etcd")`
+    """
+
     def __init__(self, location):
+        """
+        create a new etcd store, the location given will be used to generate keys
+
+        this keys will be combined to get/set instance config
+
+        Args:
+            location (Location)
+        """
         super().__init__(location, JsonSerializer())
         etcd_config = self.config_env.get_store_config("etcd")
         hostname = etcd_config.get("hostname", "localhost")
@@ -15,9 +29,29 @@ class EtcdStore(EncryptedConfigStore):
         self.etcd_client = etcd3.client(host=hostname, port=port)
 
     def get_key(self, instance_name):
+        """
+        get a key for an instance
+
+        this will return a dot-separated key derived from current location
+
+        Args:
+            instance_name (str): name
+
+        Returns:
+            str: key
+        """
         return ".".join([self.location.name, instance_name])
 
     def read(self, instance_name):
+        """
+        read instance config from mongo
+
+        Args:
+            instance_name (name): name
+
+        Returns:
+            str: data
+        """
         key = self.get_key(instance_name)
         # if not self.etcd_client.get(instance_name)[0]:
         #     raise ConfigNotFound(f"cannot find config for {instance_name} at {key}")
@@ -29,14 +63,37 @@ class EtcdStore(EncryptedConfigStore):
         return self._process_config(config, EncryptionMode.Decrypt)
 
     def write(self, instance_name, data):
+        """
+        set data with the corresponding key for this instance
+
+        Args:
+            instance_name (str): name
+            data (str): data
+
+        Returns:
+            bool: written or not
+        """
         key = self.get_key(instance_name)
         self.etcd_client.put(key, data)
-        "folder1.folder2.instance_name"
 
     def list_all(self):
+        """
+        get all names of instances (instance keys)
+
+        Returns:
+            List[str]: list of instance names
+        """
         return [item[1].key.decode().split(".")[-1] for item in self.etcd_client.get_all()]
 
     def delete(self, instance_name):
+        """
+        delete given instance
+
+        Args:
+            instance_name (str): name
+
+        Returns:
+            bool
+        """
         key = self.get_key(instance_name)
         return self.etcd_client.delete(key)
-
